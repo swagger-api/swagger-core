b1f863c0c227959c1db17627b31f2845f0157450
Tony Tam <fehguy@gmail.com>
2011-08-02 17:48:52 -0700

    added license info

--- src/main/scala/com/wordnik/swagger/core/HelpApi.scala
+++ src/main/scala/com/wordnik/swagger/core/HelpApi.scala
@@ -1,20 +1,32 @@
+/**
+ *  Copyright 2011 Wordnik, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.wordnik.swagger.core
 
-/**
-  * @author ayush
-  * @since 6/23/11 12:49 PM
-  *
-  */
+import org.slf4j.LoggerFactory
+import util.TypeUtil
 
+import org.apache.commons.lang.StringUtils
+
+import javax.ws.rs.core.{UriInfo, HttpHeaders}
+import java.rmi.server.Operation
 
 import scala.collection._
 import mutable._
 import scala.collection.JavaConversions._
-import org.slf4j.LoggerFactory
-import util.TypeUtil
-import org.apache.commons.lang.StringUtils
-import javax.ws.rs.core.{UriInfo, HttpHeaders}
-import java.rmi.server.Operation
 
 class HelpApi {
   private val LOGGER = LoggerFactory.getLogger(classOf[HelpApi])
