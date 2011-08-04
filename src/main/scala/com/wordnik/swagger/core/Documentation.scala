a3c95fa2f845eb08fe139dd7c6b544900247e8c7
Deepak Michael <mikee76@gmail.com>
2011-08-04 20:53:17 +0530

    Swagger core: Fixing issue where models were not visible in xml rendition of help

--- src/main/scala/com/wordnik/swagger/core/Documentation.scala
+++ src/main/scala/com/wordnik/swagger/core/Documentation.scala
@@ -68,7 +68,7 @@ class Documentation (@BeanProperty var apiVersion: String,
   private var _objs = new ListBuffer[DocumentationObject]
 
   @JsonIgnore
-  @XmlTransient
+  @XmlElement
   def getModels = if (_objs.size > 0) asList(_objs) else null
 
   def addModel(obj: DocumentationObject) = if (obj != null) _objs += obj
