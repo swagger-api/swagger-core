1757b8f9a28584113a6ce19a59136ca5d175f686
fehguy <tony@eatbacon.org>
2012-05-29 05:20:05 -0700

    removed local ivy repos so build works on Windows

--- samples/scala-play2/project/Build.scala
+++ samples/scala-play2/project/Build.scala
@@ -17,10 +17,6 @@ object ApplicationBuild extends Build {
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
     resolvers := Seq(
       Resolver.url("swagger-core-github-repo", url("http://wordnik.github.com/repository"))(Resolver.ivyStylePatterns),
-      Resolver.url("swagger-core-github-repo", url("https://github.com/wordnik/swagger-core/tree/master/modules/swagger-play2/repository"))(Resolver.ivyStylePatterns),
-      "local-maven-repo" at "file://" + Path.userHome.absolutePath + "/.m2/repository/",
-      Resolver.url("local-ivy", new URL("file://" + Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns),
-      Resolver.url("local-ivy-cache", new URL("file://" + Path.userHome.absolutePath + "/.ivy2/cache"))(Resolver.ivyStylePatterns),
       "java-net" at "http://download.java.net/maven/2",
       "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"))
 }
