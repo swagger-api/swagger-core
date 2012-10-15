21e144ad365f8e615860cafe566e6d52bde5a220
Ayush Gupta <ayush@glugbot.com>
2012-10-15 12:18:28 +0530

    updated play2 modules to play-2.0.4 and removed local m2 repo which was causing unpredictable errors

--- modules/swagger-play2-utils/project/plugins.sbt
+++ modules/swagger-play2-utils/project/plugins.sbt
@@ -3,5 +3,5 @@ resolvers ++= Seq(
     Resolver.url("Play", url("http://download.playframework.org/ivy-releases/"))(Resolver.ivyStylePatterns)
 )
 
-addSbtPlugin("play" % "sbt-plugin" % "2.0.2")
+addSbtPlugin("play" % "sbt-plugin" % "2.0.4")
 
--- modules/swagger-play2/project/Build.scala
+++ modules/swagger-play2/project/Build.scala
@@ -17,7 +17,6 @@ object ApplicationBuild extends Build {
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
     organization := "com.wordnik",
     resolvers := Seq(
-      "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
       Resolver.url("swagger-core-github-repo", url("http://wordnik.github.com/repository"))(Resolver.ivyStylePatterns),
       "sonatype-snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
       "sonatype-releases" at "https://oss.sonatype.org/content/repositories/releases",
