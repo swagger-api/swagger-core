85bea97131c9e359866e6e207381e0524bd8dc3a
Tony Tam <fehguy@gmail.com>
2012-11-26 17:40:47 -0800

    added publish to sonatype

--- modules/swagger-play2/project/Build.scala
+++ modules/swagger-play2/project/Build.scala
@@ -15,9 +15,45 @@ object ApplicationBuild extends Build {
     "javax.ws.rs" % "jsr311-api" % "1.1.1")
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    publishArtifact in Test := false,
+    publishMavenStyle := true,
+    pomIncludeRepository := { x => false },
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
     organization := "com.wordnik",
+    pomExtra := (
+  <url>http://swagger.wordnik.com</url>
+  <licenses>
+    <license>
+      <name>Apache License 2.0</name>
+      <url>http://www.apache.org/licenses/LICENSE-2.0.html</url>
+      <distribution>repo</distribution>
+    </license>
+  </licenses>
+  <scm>
+    <url>git@github.com:wordnik/swagger-core.git</url>
+    <connection>scm:git:git@github.com:wordnik/swagger-core.git</connection>
+  </scm>
+  <developers>
+    <developer>
+      <id>fehguy</id>
+      <name>Tony Tam</name>
+      <email>fehguy@gmail.com</email>
+    </developer>
+    <developer>
+      <id>ayush</id>
+      <name>Ayush Gupta</name>
+      <email>ayush@glugbot.com</email>
+    </developer>
+  </developers>)
+    ,
     resolvers := Seq(
-      Resolver.url("swagger-core-github-repo", url("http://wordnik.github.com/repository"))(Resolver.ivyStylePatterns),
       "sonatype-snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
       "sonatype-releases" at "https://oss.sonatype.org/content/repositories/releases",
       "java-net" at "http://download.java.net/maven/2",
