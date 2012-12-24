6b644eeeb60dbfc20bd47f5cc196fe60228f3f8b
Tony Tam <fehguy@gmail.com>
2012-12-23 17:56:27 -0800

    updated to play 2.1

--- modules/swagger-play2-utils/project/Build.scala
+++ modules/swagger-play2-utils/project/Build.scala
@@ -4,12 +4,12 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
   val appName = "swagger-play2-utils"
-  val appVersion = "1.2.0"
+  val appVersion = "1.2.1-SNAPSHOT"
 
   val appDependencies: Seq[sbt.ModuleID] = Seq(
     "org.slf4j" % "slf4j-api" % "1.6.4",
-    "com.wordnik" %% "swagger-play2" % "1.2.0",
-    "com.wordnik" %% "common-utils" % "1.1.4",
+    "com.wordnik" % "swagger-play2_2.10" % "1.2.1-SNAPSHOT",
+    "com.wordnik" % "common-utils_2.10.0" % "1.1.5",
     "javax.ws.rs" % "jsr311-api" % "1.1.1")
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
