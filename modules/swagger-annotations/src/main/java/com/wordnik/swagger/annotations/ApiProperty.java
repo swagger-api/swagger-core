3f95978bf3e386fe7e2daf126e7ddc6201882e88
redlab <balder@redlab.be>
2013-02-27 00:00:08 +0100

    added javadoc

--- modules/swagger-annotations/src/main/java/com/wordnik/swagger/annotations/ApiProperty.java
+++ modules/swagger-annotations/src/main/java/com/wordnik/swagger/annotations/ApiProperty.java
@@ -27,25 +27,25 @@ import java.lang.annotation.Target;
 public @interface ApiProperty {
 	/** Provide a human readable synopsis of this property */
     String value() default "";
-	
+
 	/**
 	 * If the values that can be set are restricted, they can be set here. In the form of a comma separated list
 	 * <code>registered, active, closed</code>.
-	 * 
+	 *
 	 * @return the allowable values
 	 */
     String allowableValues() default "";
     String access() default "";
 	/** Provide any extra information */
     String notes() default "";
+
 	/**
-	 * The dataType. See the documentation for the supported datatypes. If the data type is a custom object, set it's
-	 * name.
-	 */
-    String dataType() default "";
-	/**
+	 * The dataType. See the documentation for the supported datatypes. If the data type is a custom object, set
+	 * it's name, or nothing. In case of an enum use 'string' and allowableValues for the enum constants.
+	 * /**
 	 * Whether or not the property is required, defaults to false.
+	 * 
 	 * @return true if required, false otherwise
 	 */
-    boolean required() default false;
+	boolean required() default false;
 }
