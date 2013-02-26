402c0c8f268dbb610ba54bc31d1e75cba4f36259
redlab <balder@redlab.be>
2013-02-26 18:58:14 +0100

    added some Javadoc

--- modules/swagger-annotations/src/main/java/com/wordnik/swagger/annotations/ApiProperty.java
+++ modules/swagger-annotations/src/main/java/com/wordnik/swagger/annotations/ApiProperty.java
@@ -21,13 +21,29 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/** ApiProperty can be put on a Method to allow swagger to understand the json fields datatype and more. */
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ApiProperty {
+	/** Provide a human readable synopsis of this property */
     String value() default "";
+	/**
+	 * If the values that can be set are restricted, they can be set here. In the form of a comma separated list
+	 * <code>1-registered,2-active,3-closed</code>.
+	 * @return the allowable values
+	 */
     String allowableValues() default "";
     String access() default "";
+	/** Provide any extra information */
     String notes() default "";
+	/**
+	 * The dataType. See the documentation for the supported datatypes. If the data type is a custom object, set it's
+	 * name.
+	 */
     String dataType() default "";
+	/**
+	 * Whether or not the property is required, defaults to false.
+	 * @return true if required, false otherwise
+	 */
     boolean required() default false;
 }
