9e63d2a083349b981af839b11beb5302bbefcc32
Tony Tam <fehguy@gmail.com>
2013-05-27 22:13:41 -0700

    made filter java-friendly

--- modules/swagger-jaxrs/src/main/scala/com/wordnik/swagger/jaxrs/config/FilterFactory.scala
+++ modules/swagger-jaxrs/src/main/scala/com/wordnik/swagger/jaxrs/config/FilterFactory.scala
@@ -8,6 +8,6 @@ object FilterFactory {
 }
 
 class DefaultSpecFilter extends SwaggerSpecFilter {
-  def isOperationAllowed(operation: Operation, api: ApiDescription, params: Map[String, List[String]], cookies: Map[String, String], headers: Map[String, List[String]]): Boolean = true
-  def isParamAllowed(parameter: Parameter, operation: Operation, api: ApiDescription, params: Map[String, List[String]], cookies: Map[String, String], headers: Map[String, List[String]]): Boolean = true
+  def isOperationAllowed(operation: Operation, api: ApiDescription, params: java.util.Map[String, java.util.List[String]], cookies: java.util.Map[String, String], headers: java.util.Map[String, java.util.List[String]]): Boolean = true
+  def isParamAllowed(parameter: Parameter, operation: Operation, api: ApiDescription, params: java.util.Map[String, java.util.List[String]], cookies: java.util.Map[String, String], headers: java.util.Map[String, java.util.List[String]]): Boolean = true
 }
--- modules/swagger-jaxrs/src/main/scala/com/wordnik/swagger/jaxrs/filter/JaxrsFilter.scala
+++ modules/swagger-jaxrs/src/main/scala/com/wordnik/swagger/jaxrs/filter/JaxrsFilter.scala
@@ -4,6 +4,6 @@ import com.wordnik.swagger.core.filter.SwaggerSpecFilter
 import com.wordnik.swagger.model._
 
 class JaxrsFilter extends SwaggerSpecFilter {
-  def isOperationAllowed(operation: Operation, api: ApiDescription, params: Map[String, List[String]], cookies: Map[String, String], headers: Map[String, List[String]]): Boolean = true
-  def isParamAllowed(parameter: Parameter, operation: Operation, api: ApiDescription, params: Map[String, List[String]], cookies: Map[String, String], headers: Map[String, List[String]]): Boolean = true
+  def isOperationAllowed(operation: Operation, api: ApiDescription, params: java.util.Map[String, java.util.List[String]], cookies: java.util.Map[String, String], headers: java.util.Map[String, java.util.List[String]]): Boolean = true
+  def isParamAllowed(parameter: Parameter, operation: Operation, api: ApiDescription, params: java.util.Map[String, java.util.List[String]], cookies: java.util.Map[String, String], headers: java.util.Map[String, java.util.List[String]]): Boolean = true
 }
