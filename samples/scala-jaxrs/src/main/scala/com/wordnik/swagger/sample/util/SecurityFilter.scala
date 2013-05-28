80e8f938acad3e4a05a5c617db79273b1ce94641
Tony Tam <fehguy@gmail.com>
2013-05-27 22:13:50 -0700

    made filter java-friendly

--- samples/scala-jaxrs/src/main/scala/com/wordnik/swagger/sample/util/SecurityFilter.scala
+++ samples/scala-jaxrs/src/main/scala/com/wordnik/swagger/sample/util/SecurityFilter.scala
@@ -7,24 +7,24 @@ import javax.servlet.ServletConfig
 import javax.servlet.http.HttpServlet
 
 class SecurityFilter extends SwaggerSpecFilter {
-  def isOperationAllowed(operation: Operation, api: ApiDescription, params: Map[String, List[String]], cookies: Map[String, String], headers: Map[String, List[String]]): Boolean = {
+  def isOperationAllowed(operation: Operation, api: ApiDescription, params: java.util.Map[String, java.util.List[String]], cookies: java.util.Map[String, String], headers: java.util.Map[String, java.util.List[String]]): Boolean = {
   	val isAuthorized = checkKey(params, headers)
   	if((operation.httpMethod != "GET" && !isAuthorized) || api.path.indexOf("/store") != -1) false
   	else true
   }
 
-  def isParamAllowed(parameter: Parameter, operation: Operation, api: ApiDescription, params: Map[String, List[String]], cookies: Map[String, String], headers: Map[String, List[String]]): Boolean = {
+  def isParamAllowed(parameter: Parameter, operation: Operation, api: ApiDescription, params: java.util.Map[String, java.util.List[String]], cookies: java.util.Map[String, String], headers: java.util.Map[String, java.util.List[String]]): Boolean = {
   	val isAuthorized = checkKey(params, headers)	
   	if(parameter.paramAccess == Some("internal") && !isAuthorized) false
   	else true
   }
 
-  def checkKey(params: Map[String, List[String]], headers: Map[String, List[String]]): Boolean = {
-  	val apiKey = params.contains("api_key") match {
-			case true => Some(params("api_key")(0))
+  def checkKey(params: java.util.Map[String, java.util.List[String]], headers: java.util.Map[String, java.util.List[String]]): Boolean = {
+  	val apiKey = params.containsKey("api_key") match {
+			case true => Some(params.get("api_key").get(0))
   		case _ => {
-  			headers.contains("api_key") match {
-  				case true => Some(headers("api_key")(0))
+  			headers.containsKey("api_key") match {
+  				case true => Some(headers.get("api_key").get(0))
   				case _ => None
   			}
   		}
