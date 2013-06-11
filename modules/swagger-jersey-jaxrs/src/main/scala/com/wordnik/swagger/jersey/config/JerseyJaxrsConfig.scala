e096c15804e3e67a1547b5cd6880a9b22e6fe9b0
Tony Tam <fehguy@gmail.com>
2013-06-11 09:58:54 -0700

    moved some classes to swagger-core

--- modules/swagger-jersey-jaxrs/src/main/scala/com/wordnik/swagger/jersey/config/JerseyJaxrsConfig.scala
+++ modules/swagger-jersey-jaxrs/src/main/scala/com/wordnik/swagger/jersey/config/JerseyJaxrsConfig.scala
@@ -3,7 +3,7 @@ package com.wordnik.swagger.jersey.config
 import com.wordnik.swagger.jaxrs.config._
 
 import com.wordnik.swagger.jersey.JerseyApiReader
-import com.wordnik.swagger.config.ConfigFactory
+import com.wordnik.swagger.config.{ ConfigFactory, ScannerFactory }
 import com.wordnik.swagger.reader._
 
 import javax.servlet.ServletConfig
