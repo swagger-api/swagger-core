5715b0acf29dce49760e0f1c887b801c8b47d522
Tony Tam <fehguy@gmail.com>
2013-06-15 12:47:19 -0700

    updated auth descritpion

--- samples/scala-jaxrs/src/main/scala/com/wordnik/swagger/sample/Bootstrap.scala
+++ samples/scala-jaxrs/src/main/scala/com/wordnik/swagger/sample/Bootstrap.scala
@@ -7,7 +7,22 @@ import com.wordnik.swagger.model._
 import javax.servlet.http.HttpServlet
 
 class Bootstrap extends HttpServlet {
-  val oauth = OAuth("http://localhost:8002/oauth/requestToken", "http://localhost:8002/oauth/token", List("PUBLIC"), List("IMPLICIT"))
+  val oauth = OAuth(
+    List("PUBLIC"),
+    List(
+      ImplicitGrant(
+        LoginEndpoint("http://localhost:8002/oauth/dialog"),
+        "access_code"
+      ),
+      AuthorizationCodeGrant(
+        TokenRequestEndpoint("http://localhost:8002/oauth/requestToken",
+          "client_id",
+          "client_secret"),
+        TokenEndpoint("http://localhost:8002/oauth/token",
+          "access_code"
+        )
+    )
+  ))
   val apikey = ApiKey("api_key")
   ConfigFactory.config.authorizations = List(oauth, apikey)
 }
