9eb876957c5d919c24b300d5d56a55edfbbcf2f2
Tony Tam <fehguy@gmail.com>
2013-07-19 22:09:30 -0700

    import cleanup

--- modules/swagger-jersey-jaxrs/src/main/scala/com/wordnik/swagger/jersey/listing/JerseyProviders.scala
+++ modules/swagger-jersey-jaxrs/src/main/scala/com/wordnik/swagger/jersey/listing/JerseyProviders.scala
@@ -2,11 +2,10 @@ package com.wordnik.swagger.jersey.listing
 
 import com.wordnik.swagger.jaxrs.listing._
 
-import javax.ws.rs.core.{ MultivaluedMap, MediaType }
-import javax.ws.rs.ext.{ Provider, MessageBodyWriter }
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.ext.Provider
 import javax.ws.rs.Produces
 
-import java.lang.reflect.Type
 import java.io._
 
 @Produces(Array(MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML))
