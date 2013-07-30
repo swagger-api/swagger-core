41592460622d074b3b3d4c68661736f2b3a30ce5
Tony Tam <fehguy@gmail.com>
2013-07-30 08:17:02 -0700

    added oauth module

---
+++ modules/swagger-oauth2-auth-server/src/main/scala/com/wordnik/swagger/auth/service/TokenScope.java
@@ -0,0 +1,17 @@
+package com.wordnik.swagger.auth.service;
+
+public class TokenScope {
+  public static final ThreadLocal userThreadLocal = new ThreadLocal();
+
+  public static void setUserId(Long userId) {
+    userThreadLocal.set(userId);
+  }
+
+  public static Long getUserId() {
+    return (Long)userThreadLocal.get();
+  }
+
+  public static void unsetUserId() {
+    userThreadLocal.remove();
+  }
+}
