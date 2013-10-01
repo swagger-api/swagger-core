7cd5f98ec4adaa098dc1d962658d062980de2dd0
Tony Tam <fehguy@gmail.com>
2013-10-01 09:23:39 -0700

    added custom filter, api info to sample

--- samples/java-jaxrs/src/main/java/com/wordnik/swagger/sample/Bootstrap.java
+++ samples/java-jaxrs/src/main/java/com/wordnik/swagger/sample/Bootstrap.java
@@ -1,8 +1,9 @@
 package com.wordnik.swagger.sample;
 
-import com.wordnik.swagger.jaxrs.*;
-import com.wordnik.swagger.config.*;
 import com.wordnik.swagger.model.*;
+import com.wordnik.swagger.config.*;
+import com.wordnik.swagger.sample.util.*;
+import com.wordnik.swagger.config.FilterFactory;
 
 import javax.servlet.http.HttpServlet;
 
@@ -10,5 +11,21 @@ public class Bootstrap extends HttpServlet {
   static {
     // do any additional initialization here, such as set your base path programmatically as such:
     // ConfigFactory.config().setBasePath("http://www.foo.com/");
+
+    // add a custom filter
+    FilterFactory.setFilter(new CustomFilter());
+
+    ApiInfo info = new ApiInfo(
+      "Swagger Sample App",                             /* title */
+      "This is a sample server Petstore server.  You can find out more about Swagger " + 
+      "at <a href=\"http://swagger.wordnik.com\">http://swagger.wordnik.com</a> or on irc.freenode.net, #swagger.  For this sample, " + 
+      "you can use the api key \"special-key\" to test the authorization filters", 
+      "http://helloreverb.com/terms/",                  /* TOS URL */
+      "apiteam@wordnik.com",                            /* Contact */
+      "Apache 2.0",                                     /* license */
+      "http://www.apache.org/licenses/LICENSE-2.0.html" /* license URL */
+    );
+
+    ConfigFactory.config().setApiInfo(info);
   }
 }
