1675f692ba9029a5eb0db085e1785ef7d492be78
Andy Olliver <andy@artirix.com>
2013-10-08 08:30:55 +0100

    update swagger-play2-utils for play 2.2 and swagger 1.3 support

--- modules/swagger-play2-utils/app/controllers/HealthController.scala
+++ modules/swagger-play2-utils/app/controllers/HealthController.scala
@@ -1,52 +1,45 @@
 package controllers
 
-import com.wordnik.swagger.core._
 import com.wordnik.swagger.annotations._
-import com.wordnik.swagger.core.util.JsonUtil
+import com.wordnik.swagger.core.util.ScalaJsonUtil
+import com.wordnik.util.perf.{ HealthSnapshot, Health }
 
 import org.slf4j.LoggerFactory
 
-import play.api._
 import play.api.mvc._
-import play.api.data._
-import play.api.data.format.Formats._
-import play.api.data.validation.Constraints._
-import play.api.Logger
 
-import com.wordnik.util.perf.{ HealthSnapshot, Health, Memory }
+import javax.ws.rs.Path
 
-import javax.xml.bind.JAXBContext
-import java.io.StringWriter
-import javax.ws.rs.{ Path, QueryParam }
 
-import scala.reflect.BeanProperty
-import scala.collection.JavaConversions._
-
-@Api(value = "/admin", listingPath = "/api-docs.{format}/admin", description = "Administrative operations")
-object HealthController extends SwaggerBaseApiController {
-  override protected def jaxbContext(): JAXBContext = JAXBContext.newInstance(classOf[Health])
+@Api(value = "/admin", description = "Administrative operations")
+object HealthController extends Controller {
   val LOGGER = LoggerFactory.getLogger(HealthController.getClass)
+  val AccessControlAllowOrigin = ("Access-Control-Allow-Origin", "*")
 
   @Path("/health")
   @ApiOperation(value = "Returns health report on this JVM",
-    responseClass = "com.wordnik.util.perf.Health",
+    response = classOf[com.wordnik.util.perf.Health],
     httpMethod = "GET")
   def getHealth() = Action { request =>
     try {
       val health: Health = HealthSnapshot.get()
-      returnValue(request, health)
+
+      new SimpleResult(header = ResponseHeader(200), body = play.api.libs.iteratee.Enumerator(ScalaJsonUtil.mapper.writeValueAsBytes(health))).as("application/json")
+        .withHeaders(AccessControlAllowOrigin)
     } catch {
-      case e: Exception => LOGGER.error("Error occured", e); InternalServerError //Error(e.getMessage)
+      case e: Exception => LOGGER.error("Error occurred", e); InternalServerError //Error(e.getMessage)
     }
   }
 
   @Path("/ping")
-  @ApiOperation(value = "Pings service", responseClass = "String", httpMethod = "GET")
+  @ApiOperation(value = "Pings service", response = classOf[String],
+    produces = "text/plain", httpMethod = "GET")
   def ping() = Action { request =>
     try {
-      returnValue(request, ok)
+      new SimpleResult(header = ResponseHeader(200), body = play.api.libs.iteratee.Enumerator("OK".getBytes)).as("text/plain")
+        .withHeaders(AccessControlAllowOrigin)
     } catch {
-      case e: Exception => LOGGER.error("Error occured", e); InternalServerError //Error(e.getMessage)
+      case e: Exception => LOGGER.error("Error occurred", e); InternalServerError //Error(e.getMessage)
     }
   }
 }
