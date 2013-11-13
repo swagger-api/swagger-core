908c3428176bf94f89ad438cbd45d211500dc177
Matt Thomson <matt.thomson@mendeley.com>
2013-11-13 12:35:01 +0000

    Fix serialization of set fields
    JsonSchemaModelSerializer has a toJsonSchemaContainer method, which produces
    documentation for container types.  However, sets are not documented correctly
    as Set is not one of the container types in SwaggerSpec.  Fix is simply to add
    Set in there.
    
    Includes test.

--- modules/swagger-core/src/main/scala/com/wordnik/swagger/core/SwaggerSpec.scala
+++ modules/swagger-core/src/main/scala/com/wordnik/swagger/core/SwaggerSpec.scala
@@ -2,6 +2,6 @@ package com.wordnik.swagger.core
 
 object SwaggerSpec {
   val baseTypes = Set("byte", "boolean", "int", "long", "float", "double", "string", "date", "void", "Date")
-  val containerTypes = Set("Array", "List")
+  val containerTypes = Set("Array", "List", "Set")
   val version = "1.2"
 }
