b5e5d18038dbb209da5f44e265d3730664369f12
Tony Tam <fehguy@gmail.com>
2014-01-05 11:02:07 -0800

    updated versions

--- samples/scala-play2/project/Build.scala
+++ samples/scala-play2/project/Build.scala
@@ -8,13 +8,14 @@ object ApplicationBuild extends Build {
   val appVersion = "1.3-SNAPSHOT"
 
   val appDependencies: Seq[sbt.ModuleID] = Seq(
-    "com.wordnik" %% "swagger-play2" % "1.3-SNAPSHOT",
-    "com.wordnik" %% "swagger-play2-utils" % "1.3-SNAPSHOT"
+    "com.wordnik" %% "swagger-play2" % "1.3.2-SNAPSHOT",
+    "com.wordnik" %% "swagger-play2-utils" % "1.3.2-SNAPSHOT"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     resolvers := Seq(
       "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
+      Resolver.url("Local Ivy Repository", url("file://"+Path.userHome.absolutePath+"/.ivy2/local"))(Resolver.ivyStylePatterns),
       "sonatype-snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
       "sonatype-releases" at "https://oss.sonatype.org/content/repositories/releases",
       "java-net" at "http://download.java.net/maven/2",
