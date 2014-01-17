3459439324fbd8cb2187f362a16a7a796c4c6a0b
Tony Tam <fehguy@gmail.com>
2014-01-16 23:23:10 -0800

    updated snapshot to 1.3.3

--- modules/swagger-play2/project/Build.scala
+++ modules/swagger-play2/project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
   val appName = "swagger-play2"
-  val appVersion = "1.3.2-SNAPSHOT"
+  val appVersion = "1.3.3-SNAPSHOT"
 
   checksums in update := Nil
 
@@ -12,9 +12,9 @@ object ApplicationBuild extends Build {
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.1.3",
     "com.fasterxml.jackson.core" % "jackson-annotations" % "2.1.4",
     "org.slf4j" % "slf4j-api" % "1.6.4",
-    "com.wordnik" % "swagger-core_2.10" % "1.3.2-SNAPSHOT",
-    "com.wordnik" % "swagger-annotations" % "1.3.2-SNAPSHOT",
-    "com.wordnik" % "swagger-jaxrs_2.10" % "1.3.2-SNAPSHOT",
+    "com.wordnik" % "swagger-core_2.10" % "1.3.3-SNAPSHOT",
+    "com.wordnik" % "swagger-annotations" % "1.3.3-SNAPSHOT",
+    "com.wordnik" % "swagger-jaxrs_2.10" % "1.3.3-SNAPSHOT",
     "javax.ws.rs" % "jsr311-api" % "1.1.1",
     "org.mockito" % "mockito-core" % "1.9.5" % "test")
 
--- samples/scala-play2/project/Build.scala
+++ samples/scala-play2/project/Build.scala
@@ -5,11 +5,11 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName = "petstore"
-  val appVersion = "1.3.2-SNAPSHOT"
+  val appVersion = "1.3.3-SNAPSHOT"
 
   val appDependencies: Seq[sbt.ModuleID] = Seq(
-    "com.wordnik" %% "swagger-play2" % "1.3.2-SNAPSHOT",
-    "com.wordnik" %% "swagger-play2-utils" % "1.3.2-SNAPSHOT"
+    "com.wordnik" %% "swagger-play2" % "1.3.3-SNAPSHOT",
+    "com.wordnik" %% "swagger-play2-utils" % "1.3.3-SNAPSHOT"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
