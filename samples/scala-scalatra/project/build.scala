12ba6c551ea28350abb4bb8422442de384a09d65
Markus Krogemann <markus@krogemann.de>
2014-02-16 18:03:13 +0100

    add missing swagger-annotation dependency

--- samples/scala-scalatra/project/build.scala
+++ samples/scala-scalatra/project/build.scala
@@ -21,11 +21,12 @@ object PetstoreBuild extends Build {
       resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
       libraryDependencies ++= Seq(
         "org.scalatra" %% "scalatra" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-swagger" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-specs2"  % ScalatraVersion % "test",
-        "org.json4s"   %% "json4s-jackson"   % "3.1.0",
-        "org.json4s"   %% "json4s-ext"       % "3.1.0",
+        "org.scalatra" %% "scalatra-scalate"    % ScalatraVersion,
+        "org.scalatra" %% "scalatra-swagger"    % ScalatraVersion,
+        "org.scalatra" %% "scalatra-specs2"     % ScalatraVersion % "test",
+        "org.json4s"   %% "json4s-jackson"      % "3.1.0",
+        "org.json4s"   %% "json4s-ext"          % "3.1.0",
+        "com.wordnik"  %  "swagger-annotations" % "1.3.2",
 
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
