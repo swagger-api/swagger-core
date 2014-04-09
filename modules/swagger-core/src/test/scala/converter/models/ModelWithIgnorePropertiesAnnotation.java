e55630dc28a45a6e7f039db01131e4a6cde5787e
Andy Hawkes <andy@andyhawkes.com>
2014-04-08 21:12:48 -0700

    #519 New test case to test whether @JsonIgnoreProperties annotation is respected by Swagger.

---
+++ modules/swagger-core/src/test/scala/converter/models/ModelWithIgnorePropertiesAnnotation.java
@@ -0,0 +1,30 @@
+package converter.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"ignoredString", "middleName"})
+public class ModelWithIgnorePropertiesAnnotation {
+  private String name, ignoredString;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getIgnoredString() {
+    return ignoredString;
+  }
+
+  public void setIgnoredString(String ignoredString) {
+    this.ignoredString = ignoredString;
+  }
+
+  public String getMiddleName() {
+    return "ignore middle name";
+  }
+
+  public void setMiddleName(String name) {}
+}
