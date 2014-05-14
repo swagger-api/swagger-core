42b7812350360152da964bc20dd0fe504be6e2bf
Andrey Saxonov <Andrey.Saxonov@DataArt.com>
2014-05-14 21:11:13 +0400

    Source compatibility with Play 2.3

--- modules/swagger-play2/project/Build.scala
+++ modules/swagger-play2/project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
   val appName = "swagger-play2"
-  val appVersion = "1.3.5"
+  val appVersion = "1.3.6-SNAPSHOT"
 
   checksums in update := Nil
 
