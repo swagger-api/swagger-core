33ed9c254448b9226c647dbefe9ac8fbc55949d7
Lloyd <lloydmeta@gmail.com>
2014-05-30 14:20:12 +0900

    Fix spacing

--- modules/swagger-play2-utils/app/controllers/HealthController.scala
+++ modules/swagger-play2-utils/app/controllers/HealthController.scala
@@ -19,7 +19,7 @@ object HealthController extends Controller {
   @Path("/health")
   @ApiOperation(value = "Returns health report on this JVM",
     response = classOf[com.wordnik.util.perf.Health],
-    nickname= "Health endpoint",
+    nickname = "Health endpoint",
     httpMethod = "GET")
   def getHealth() = Action { request =>
     try {
@@ -35,7 +35,7 @@ object HealthController extends Controller {
   @Path("/ping")
   @ApiOperation(value = "Pings service",
     response = classOf[String],
-    nickname= "Ping endpoint",
+    nickname = "Ping endpoint",
     produces = "text/plain",
     httpMethod = "GET")
   def ping() = Action { request =>
