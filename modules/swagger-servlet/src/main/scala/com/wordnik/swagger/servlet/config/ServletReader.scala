3885e5518211ab53cba218814d2e0053ce37825a
Tony Tam <fehguy@gmail.com>
2014-07-12 19:44:43 -0700

    fix for #604, added description from annotations

--- modules/swagger-servlet/src/main/scala/com/wordnik/swagger/servlet/config/ServletReader.scala
+++ modules/swagger-servlet/src/main/scala/com/wordnik/swagger/servlet/config/ServletReader.scala
@@ -51,7 +51,7 @@ class ServletReader extends ClassReader with ClassReaderUtils {
                 val allowableValues = toAllowableValues(param.allowableValues)
                 Parameter(
                   param.name,
-                  None,
+                  Option(param.value),
                   Option(param.defaultValue).filter(_.trim.nonEmpty),
                   param.required,
                   param.allowMultiple,
