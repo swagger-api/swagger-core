ed63ac07893ce891356f8a5a8b2142250edd526d
Tony Tam <fehguy@gmail.com>
2014-07-22 18:50:32 -0400

    updated common-utils

--- modules/swagger-play2-utils/project/Build.scala
+++ modules/swagger-play2-utils/project/Build.scala
@@ -12,8 +12,8 @@ object ApplicationBuild extends Build {
 
   val appDependencies: Seq[sbt.ModuleID] = Seq(
     "org.slf4j" % "slf4j-api" % "1.6.4",
-    "com.wordnik" % "swagger-core_2.10" % "1.3.8-SNAPSHOT",
-    "com.wordnik" % "common-utils_2.10.0" % "1.1.5",
+    "com.wordnik" %% "swagger-core" % "1.3.8-SNAPSHOT",
+    "com.wordnik" %% "common-utils" % "1.3.0-SNAPSHOT",
     "javax.ws.rs" % "jsr311-api" % "1.1.1")
 
   val main = Project(appName, file(".")).enablePlugins(PlayScala).settings(
