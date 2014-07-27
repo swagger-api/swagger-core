44eb845cfb4a07e1963a2b2c016bda89bca0cffd
Tony Tam <fehguy@gmail.com>
2014-07-26 19:23:49 -0700

    updated sample

--- samples/scala-play2/project/Build.scala
+++ samples/scala-play2/project/Build.scala
@@ -1,6 +1,8 @@
 import sbt._
 import Keys._
-import play.Project._
+import play.Play.autoImport._
+import play.PlayScala
+import PlayKeys._
 
 object ApplicationBuild extends Build {
   val appName = "petstore"
@@ -11,7 +13,9 @@ object ApplicationBuild extends Build {
     "com.wordnik" %% "swagger-play2-utils" % "1.3.8-SNAPSHOT"
   )
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
+  val main = Project(appName, file(".")).enablePlugins(play.PlayScala).settings(
+    version := appVersion,
+    libraryDependencies ++= appDependencies,
     resolvers := Seq(
       "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
       Resolver.url("Local Ivy Repository", url("file://"+Path.userHome.absolutePath+"/.ivy2/local"))(Resolver.ivyStylePatterns),
