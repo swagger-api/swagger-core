677178de7fe0e2ea45c14d233cc0450d9df62a8b
Tony Tam <fehguy@gmail.com>
2014-08-04 01:28:02 -0400

    removed old files

--- modules/swagger-jaxrs/src/main/scala/com/wordnik/swagger/jaxrs/filter/JaxrsFilter.scala
+++
@@ -1,9 +0,0 @@
-package com.wordnik.swagger.jaxrs.filter
-
-import com.wordnik.swagger.core.filter.SwaggerSpecFilter
-import com.wordnik.swagger.model._
-
-class JaxrsFilter extends SwaggerSpecFilter {
-  def isOperationAllowed(operation: Operation, api: ApiDescription, params: java.util.Map[String, java.util.List[String]], cookies: java.util.Map[String, String], headers: java.util.Map[String, java.util.List[String]]): Boolean = true
-  def isParamAllowed(parameter: Parameter, operation: Operation, api: ApiDescription, params: java.util.Map[String, java.util.List[String]], cookies: java.util.Map[String, String], headers: java.util.Map[String, java.util.List[String]]): Boolean = true
-}
