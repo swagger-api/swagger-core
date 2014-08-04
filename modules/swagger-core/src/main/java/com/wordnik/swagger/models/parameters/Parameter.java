606b6c1d7186eb2d8122310e2473b7966e912138
Tony Tam <fehguy@gmail.com>
2014-08-04 08:09:18 -0400

    moved to interface

--- modules/swagger-core/src/main/java/com/wordnik/swagger/models/parameters/Parameter.java
+++ modules/swagger-core/src/main/java/com/wordnik/swagger/models/parameters/Parameter.java
@@ -4,7 +4,9 @@ import com.wordnik.swagger.models.properties.Property;
 
 public interface Parameter {
   void setName(String name);
-
+  String getIn();
+  String getName();
   String getDescription();
+  boolean getRequired();
   void setDescription(String description);
 }
