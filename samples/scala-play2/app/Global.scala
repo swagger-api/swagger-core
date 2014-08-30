a20994d26c6cc9af8cc30b64d68f209b5cafe0a2
Tony Tam <fehguy@gmail.com>
2014-08-29 22:01:03 -0700

    fix for #607

--- samples/scala-play2/app/Global.scala
+++ samples/scala-play2/app/Global.scala
@@ -5,6 +5,16 @@ import com.wordnik.swagger.model._
 
 
 object Global extends GlobalSettings {
+  val info = ApiInfo(
+    title = "Swagger Sample App",
+    description = """This is a sample server Petstore server.  You can find out more about Swagger 
+    at <a href="http://swagger.wordnik.com">http://swagger.wordnik.com</a> or on irc.freenode.net, #swagger.  For this sample,
+    you can use the api key "special-key" to test the authorization filters""", 
+    termsOfServiceUrl = "http://helloreverb.com/terms/",
+    contact = "apiteam@wordnik.com", 
+    license = "Apache 2.0", 
+    licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0.html")
+
   val oauth = OAuth(
     List(
       AuthorizationScope("write:pets", "Modify pets in your account"),
@@ -24,16 +34,5 @@ object Global extends GlobalSettings {
     )
   ))
   ConfigFactory.config.authorizations = List(oauth)
-
-  val info = ApiInfo(
-    title = "Swagger Sample App",
-    description = """This is a sample server Petstore server.  You can find out more about Swagger 
-    at <a href="http://swagger.wordnik.com">http://swagger.wordnik.com</a> or on irc.freenode.net, #swagger.  For this sample,
-    you can use the api key "special-key" to test the authorization filters""", 
-    termsOfServiceUrl = "http://helloreverb.com/terms/",
-    contact = "apiteam@wordnik.com", 
-    license = "Apache 2.0", 
-    licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0.html")
-
   ConfigFactory.config.info = Some(info)
 }
