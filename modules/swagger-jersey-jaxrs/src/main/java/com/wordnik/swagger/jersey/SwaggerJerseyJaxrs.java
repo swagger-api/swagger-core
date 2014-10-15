245a6e21987d004de69f3474d20062cb6a81c627
Tony Tam <fehguy@gmail.com>
2014-10-15 00:25:13 -0400

    added jersey impl

---
+++ modules/swagger-jersey-jaxrs/src/main/java/com/wordnik/swagger/jersey/SwaggerJerseyJaxrs.java
@@ -0,0 +1,31 @@
+package com.wordnik.swagger.jersey;
+
+import com.wordnik.swagger.jaxrs.ext.*;
+import com.wordnik.swagger.models.parameters.*;
+
+import com.sun.jersey.multipart.FormDataParam;
+import com.sun.jersey.api.core.InjectParam;
+
+import java.lang.annotation.Annotation;
+
+public class SwaggerJerseyJaxrs implements SwaggerExtension {
+  public Parameter processParameter(Annotation[] annotations, Class<?> cls, boolean isArray) {
+    for(Annotation annotation : annotations) {
+      if(annotation instanceof FormDataParam) {
+        FormDataParam fd = (FormDataParam) annotation;
+        if(java.io.InputStream.class.equals(cls))
+          return new FormParameter().type("file").name(fd.value());
+        else
+          return new FormParameter().name(fd.value());
+      }
+    }
+    return null;
+  }
+
+  public boolean shouldIgnoreClass(Class<?> cls) {
+    if(com.sun.jersey.core.header.FormDataContentDisposition.class.equals(cls))
+      return true;
+    else
+      return false;
+  }
+}
