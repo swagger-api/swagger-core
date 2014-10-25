afeaee49a522d24b7ac7ff4c733a6a6f5b0b1920
Tony Tam <fehguy@gmail.com>
2014-10-24 20:07:37 -0700

    added api-key

--- samples/java-jaxrs/src/main/java/com/wordnik/swagger/sample/util/ApiOriginFilter.java
+++ samples/java-jaxrs/src/main/java/com/wordnik/swagger/sample/util/ApiOriginFilter.java
@@ -28,7 +28,7 @@ public class ApiOriginFilter implements javax.servlet.Filter {
     HttpServletResponse res = (HttpServletResponse) response;
     res.addHeader("Access-Control-Allow-Origin", "*");
     res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-    res.addHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.addHeader("Access-Control-Allow-Headers", "Content-Type, api_key");
     chain.doFilter(request, response);
   }
 
