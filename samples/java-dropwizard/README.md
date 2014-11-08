8dce88fad358de47a054208b564db2f4a05834e4
Tony Tam <fehguy@gmail.com>
2014-11-08 13:25:42 -0800

    added dropwizard sample

---
+++ samples/java-dropwizard/README.md
@@ -0,0 +1,10 @@
+To run the sample:
+
+```
+mvn package
+
+java -jar target/swagger-java-dropwizard-sample-app-1.5.0-SNAPSHOT.jar server conf/swagger-sample.yml 
+
+```
+
+You can then access swagger at http://localhost:8080/api-docs
---
+++ samples/java-dropwizard/src/main/java/com/wordnik/swagger/sample/SwaggerSampleConfiguration.java
@@ -0,0 +1,16 @@
+package com.wordnik.swagger.sample;
+
+import com.yammer.dropwizard.config.Configuration;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class SwaggerSampleConfiguration extends Configuration {
+  @NotEmpty
+  @JsonProperty
+  private String defaultName = "swagger-sample";
+
+  public String getDefaultName() {
+    return defaultName;
+  }
+}
