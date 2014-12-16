8c38bdbbf272fac171f2a452bdd703a53169b5c5
reta <drreta@gmail.com>
2014-12-16 18:46:55 -0500

    Enhanced BeanConfig to support @Path JAX-RS resources. Fixed BasicJaxrsReader operations recognition

--- modules/swagger-jaxrs/src/test/scala/NonAnnotatedResourceBeanConfigTest.scala
+++ modules/swagger-jaxrs/src/test/scala/NonAnnotatedResourceBeanConfigTest.scala
@@ -1,32 +1,37 @@
 import testresources._
-
 import com.wordnik.swagger.jaxrs.reader._
 import com.wordnik.swagger.core.util._
 import com.wordnik.swagger.model._
 import com.wordnik.swagger.config._
 import com.wordnik.swagger.core.filter._
-
 import java.lang.reflect.Method
-
 import java.util.Date
-
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
-
 import scala.collection.mutable.ListBuffer
+import com.wordnik.swagger.jaxrs.config.BeanConfig
+import com.wordnik.swagger.reader.ClassReaders
+import com.wordnik.swagger.config.ConfigFactory
 
 @RunWith(classOf[JUnitRunner])
-class NonAnnotatedResourceTest extends FlatSpec with Matchers {
+class NonAnnotatedResourceBeanConfigTest extends FlatSpec with Matchers {
   it should "read a resource without Api annotations" in {
-    val reader = new BasicJaxrsReader
-    val config = new SwaggerConfig()
+    val beanConfig = new BeanConfig()
+    beanConfig.setApiReader("com.wordnik.swagger.jaxrs.reader.BasicJaxrsReader")
+    beanConfig.setResourcePackage("testresources")
+    
+    val reader =  ClassReaders.reader.get
+    val config = ConfigFactory.config
+    
+    val classes = beanConfig.classesFromContext(null, null)
+    classes should contain (classOf[NonAnnotatedResource])
     
     val apiResource = reader.read("/api-docs", classOf[NonAnnotatedResource], config).getOrElse(fail("should not be None"))
     apiResource.apis should have size 2
-
-    val apis = apiResource.apis.map(m => (m.path -> m)).toMap
+    
+    val apis = apiResource.apis.map(m => (m.path -> m)).toMap    
 
     val api1 = apis("/basic/{id}/r")
     val ops1 = api1.operations.map(m => (m.method -> m)).toMap
@@ -44,5 +49,19 @@ class NonAnnotatedResourceTest extends FlatSpec with Matchers {
 
     val models = apiResource.models.get
     val howdy = models("Howdy")    
+  }
+  
+  it should "not read a resource without Api annotations" in {
+    val beanConfig = new BeanConfig()
+    beanConfig.setResourcePackage("testresources")
+    
+    val reader =  ClassReaders.reader.get
+    val config = ConfigFactory.config
+    
+    val classes = beanConfig.classesFromContext(null, null)
+    classes should contain (classOf[NonAnnotatedResource])
+    
+    val apiResource = reader.read("/api-docs", classOf[NonAnnotatedResource], config)
+    apiResource should be === None 
   }
 }
