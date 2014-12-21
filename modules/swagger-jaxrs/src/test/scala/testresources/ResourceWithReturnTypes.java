b66e4a2159a2dffb177b324bddc2f4d526ae1c64
Tony Tam <fehguy@gmail.com>
2014-12-21 00:30:19 -0800

    fixed build issue

--- modules/swagger-jaxrs/src/test/scala/testresources/ResourceWithReturnTypes.java
+++ modules/swagger-jaxrs/src/test/scala/testresources/ResourceWithReturnTypes.java
@@ -17,9 +17,9 @@ public class ResourceWithReturnTypes {
   @ApiOperation(value = "Find by ID")
   @ApiResponses(value = { @ApiResponse(code = 400, message = "Invalid ID supplied"),
       @ApiResponse(code = 404, message = "Pet not found") })
-  public Sample getTest(
+  public JavaSample getTest(
     @ApiParam(value = "sample param data", required = true, allowableValues = "range[0,10]")@DefaultValue("1") @QueryParam("id") String id) {
-    Sample out = new Sample();
+    JavaSample out = new JavaSample();
     out.setName("foo");
     out.setValue("bar");
     return out;
