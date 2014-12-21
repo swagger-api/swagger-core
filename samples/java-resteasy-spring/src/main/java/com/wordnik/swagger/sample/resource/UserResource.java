c735a50f43658ddb520bc60d9276a68c3ad7861b
Tony Tam <fehguy@gmail.com>
2014-12-21 15:55:58 -0800

    added samples/java-resteasy-spring sample

---
+++ samples/java-resteasy-spring/src/main/java/com/wordnik/swagger/sample/resource/UserResource.java
@@ -0,0 +1,94 @@
+package com.wordnik.swagger.sample.resource;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
+import com.wordnik.swagger.sample.exception.ApiException;
+import com.wordnik.swagger.sample.model.User;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+@Path("/user")
+@Api(value="/user", description = "Operations about user")
+@Produces({"application/json", "application/xml"})
+public interface UserResource {
+    @POST
+    @ApiOperation(value = "Create user",
+            notes = "This can only be done by the logged in user.",
+            position = 1)
+    Response createUser(
+            @ApiParam(value = "Created user object", required = true) User user);
+
+    @POST
+    @Path("/createWithArray")
+    @ApiOperation(value = "Creates list of users with given input array",
+            position = 2)
+    Response createUsersWithArrayInput(@ApiParam(value = "List of user object", required = true) User[] users);
+
+    @POST
+    @Path("/createWithList")
+    @ApiOperation(value = "Creates list of users with given input array",
+            position = 3)
+    Response createUsersWithListInput(@ApiParam(value = "List of user object", required = true) java.util.List<User> users);
+
+    @PUT
+    @Path("/{username}")
+    @ApiOperation(value = "Updated user",
+            notes = "This can only be done by the logged in user.",
+            position = 4)
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Invalid user supplied"),
+            @ApiResponse(code = 404, message = "User not found")})
+    Response updateUser(
+            @ApiParam(value = "name that need to be deleted", required = true) @PathParam("username") String username,
+            @ApiParam(value = "Updated user object", required = true) User user);
+
+    @DELETE
+    @Path("/{username}")
+    @ApiOperation(value = "Delete user",
+            notes = "This can only be done by the logged in user.",
+            position = 5)
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Invalid username supplied"),
+            @ApiResponse(code = 404, message = "User not found")})
+    Response deleteUser(
+            @ApiParam(value = "The name that needs to be deleted", required = true) @PathParam("username") String username);
+
+    @GET
+    @Path("/{username}")
+    @ApiOperation(value = "Get user by user name",
+            response = User.class,
+            position = 0)
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Invalid username supplied"),
+            @ApiResponse(code = 404, message = "User not found")})
+    Response getUserByName(
+            @ApiParam(value = "The name that needs to be fetched. Use user1 for testing. ", required = true) @PathParam("username") String username)
+            throws ApiException;
+
+    @GET
+    @Path("/login")
+    @ApiOperation(value = "Logs user into the system",
+            response = String.class,
+            position = 6)
+    @ApiResponses(value = {@ApiResponse(code = 400, message = "Invalid username/password supplied")})
+    Response loginUser(
+            @ApiParam(value = "The user name for login", required = true) @QueryParam("username") String username,
+            @ApiParam(value = "The password for login in clear text", required = true) @QueryParam("password") String password);
+
+    @GET
+    @Path("/logout")
+    @ApiOperation(value = "Logs out current logged in user session",
+            position = 7)
+    Response logoutUser();
+}
