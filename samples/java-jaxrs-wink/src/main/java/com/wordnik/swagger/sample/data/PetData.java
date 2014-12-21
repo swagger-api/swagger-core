3148cf2f9106946bc702bdb4b81bd820194d956c
Tony Tam <fehguy@gmail.com>
2014-12-21 15:06:51 -0800

    renamed, updated to follow other samples

---
+++ samples/java-jaxrs-wink/src/main/java/com/wordnik/swagger/sample/data/PetData.java
@@ -0,0 +1,145 @@
+/**
+ *  Copyright 2014 Reverb Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.wordnik.swagger.sample.data;
+
+import com.wordnik.swagger.sample.model.Category;
+import com.wordnik.swagger.sample.model.Pet;
+import com.wordnik.swagger.sample.model.Tag;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class PetData {
+  static List<Pet> pets = new ArrayList<Pet>();
+  static List<Category> categories = new ArrayList<Category>();
+
+  static {
+    categories.add(createCategory(1, "Dogs"));
+    categories.add(createCategory(2, "Cats"));
+    categories.add(createCategory(3, "Rabbits"));
+    categories.add(createCategory(4, "Lions"));
+
+    pets.add(createPet(1, categories.get(1), "Cat 1", new String[] {
+        "url1", "url2" }, new String[] { "tag1", "tag2" }, "available"));
+    pets.add(createPet(2, categories.get(1), "Cat 2", new String[] {
+        "url1", "url2" }, new String[] { "tag2", "tag3" }, "available"));
+    pets.add(createPet(3, categories.get(1), "Cat 3", new String[] {
+        "url1", "url2" }, new String[] { "tag3", "tag4" }, "pending"));
+
+    pets.add(createPet(4, categories.get(0), "Dog 1", new String[] {
+        "url1", "url2" }, new String[] { "tag1", "tag2" }, "available"));
+    pets.add(createPet(5, categories.get(0), "Dog 2", new String[] {
+        "url1", "url2" }, new String[] { "tag2", "tag3" }, "sold"));
+    pets.add(createPet(6, categories.get(0), "Dog 3", new String[] {
+        "url1", "url2" }, new String[] { "tag3", "tag4" }, "pending"));
+
+    pets.add(createPet(7, categories.get(3), "Lion 1", new String[] {
+        "url1", "url2" }, new String[] { "tag1", "tag2" }, "available"));
+    pets.add(createPet(8, categories.get(3), "Lion 2", new String[] {
+        "url1", "url2" }, new String[] { "tag2", "tag3" }, "available"));
+    pets.add(createPet(9, categories.get(3), "Lion 3", new String[] {
+        "url1", "url2" }, new String[] { "tag3", "tag4" }, "available"));
+
+    pets.add(createPet(10, categories.get(2), "Rabbit 1", new String[] {
+        "url1", "url2" }, new String[] { "tag3", "tag4" }, "available"));
+  }
+
+  public Pet getPetbyId(long petId) {
+    for (Pet pet : pets) {
+      if (pet.getId() == petId) {
+        return pet;
+      }
+    }
+    return null;
+  }
+
+  public List<Pet> findPetByStatus(String status) {
+    String[] statues = status.split(",");
+    List<Pet> result = new java.util.ArrayList<Pet>();
+    for (Pet pet : pets) {
+      for (String s : statues) {
+        if (s.equals(pet.getStatus())) {
+          result.add(pet);
+        }
+      }
+    }
+    return result;
+  }
+
+  public List<Pet> findPetByTags(String tags) {
+    String[] tagList = tags.split(",");
+    List<Pet> result = new java.util.ArrayList<Pet>();
+    for (Pet pet : pets) {
+      if (null != pet.getTags()) {
+        for (Tag tag : pet.getTags()) {
+          for (String tagListString : tagList) {
+            if (tagListString.equals(tag.getName()))
+              result.add(pet);
+          }
+        }
+      }
+    }
+    return result;
+  }
+
+  public void addPet(Pet pet) {
+    if (pets.size() > 0) {
+      for (int i = pets.size() - 1; i >= 0; i--) {
+        if (pets.get(i).getId() == pet.getId()) {
+          pets.remove(i);
+        }
+      }
+    }
+    pets.add(pet);
+  }
+
+  static Pet createPet(long id, Category cat, String name, String[] urls,
+      String[] tags, String status) {
+    Pet pet = new Pet();
+    pet.setId(id);
+    pet.setCategory(cat);
+    pet.setName(name);
+    if (null != urls) {
+      List<String> urlObjs = new ArrayList<String>();
+      for (String urlString : urls) {
+        urlObjs.add(urlString);
+      }
+      pet.setPhotoUrls(urlObjs);
+    }
+    List<Tag> tagObjs = new java.util.ArrayList<Tag>();
+    int i = 0;
+    if (null != tags) {
+      for (String tagString : tags) {
+        i = i + 1;
+        Tag tag = new Tag();
+        tag.setId(i);
+        tag.setName(tagString);
+        tagObjs.add(tag);
+      }
+    }
+    pet.setTags(tagObjs);
+    pet.setStatus(status);
+    return pet;
+  }
+
+  static Category createCategory(long id, String name) {
+    Category category = new Category();
+    category.setId(id);
+    category.setName(name);
+    return category;
+  }
+}
---
+++ samples/java-jaxrs-wink/src/main/java/com/wordnik/swagger/sample/data/UserData.java
@@ -0,0 +1,96 @@
+/**
+ *  Copyright 2014 Reverb Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.wordnik.swagger.sample.data;
+
+import com.wordnik.swagger.sample.model.User;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class UserData {
+  static List<User> users = new ArrayList<User>();
+
+  static {
+    users.add(createUser(1, "user1", "first name 1", "last name 1",
+        "email1@test.com", "123-456-7890", 1));
+    users.add(createUser(2, "user2", "first name 2", "last name 2",
+        "email2@test.com", "123-456-7890", 2));
+    users.add(createUser(3, "user3", "first name 3", "last name 3",
+        "email3@test.com", "123-456-7890", 3));
+    users.add(createUser(4, "user4", "first name 4", "last name 4",
+        "email4@test.com", "123-456-7890", 1));
+    users.add(createUser(5, "user5", "first name 5", "last name 5",
+        "email5@test.com", "123-456-7890", 2));
+    users.add(createUser(6, "user6", "first name 6", "last name 6",
+        "email6@test.com", "123-456-7890", 3));
+    users.add(createUser(7, "user7", "first name 7", "last name 7",
+        "email7@test.com", "123-456-7890", 1));
+    users.add(createUser(8, "user8", "first name 8", "last name 8",
+        "email8@test.com", "123-456-7890", 2));
+    users.add(createUser(9, "user9", "first name 9", "last name 9",
+        "email9@test.com", "123-456-7890", 3));
+    users.add(createUser(10, "user10", "first name 10", "last name 10",
+        "email10@test.com", "123-456-7890", 1));
+    users.add(createUser(11, "user?10", "first name ?10", "last name ?10",
+        "email101@test.com", "123-456-7890", 1));
+
+  }
+
+  public User findUserByName(String username) {
+    for (User user : users) {
+      if (user.getUsername().equals(username)) {
+        return user;
+      }
+    }
+    return null;
+  }
+
+  public void addUser(User user) {
+    if (users.size() > 0) {
+      for (int i = users.size() - 1; i >= 0; i--) {
+        if (users.get(i).getUsername().equals(user.getUsername())) {
+          users.remove(i);
+        }
+      }
+    }
+    users.add(user);
+  }
+
+  public void removeUser(String username) {
+    if (users.size() > 0) {
+      for (int i = users.size() - 1; i >= 0; i--) {
+        if (users.get(i).getUsername().equals(username)) {
+          users.remove(i);
+        }
+      }
+    }
+  }
+
+  private static User createUser(long id, String username, String firstName,
+      String lastName, String email, String phone, int userStatus) {
+    User user = new User();
+    user.setId(id);
+    user.setUsername(username);
+    user.setFirstName(firstName);
+    user.setLastName(lastName);
+    user.setEmail(email);
+    user.setPassword("XXXXXXXXXXX");
+    user.setPhone(phone);
+    user.setUserStatus(userStatus);
+    return user;
+  }
+}
