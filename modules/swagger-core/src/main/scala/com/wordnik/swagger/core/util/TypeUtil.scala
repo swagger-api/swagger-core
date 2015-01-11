08788a347896c71b2f3fa4ac527bada303997f1f
Tony Tam <fehguy@gmail.com>
2015-01-10 22:28:16 -0800

    added collection support

--- modules/swagger-core/src/main/scala/com/wordnik/swagger/core/util/TypeUtil.scala
+++ modules/swagger-core/src/main/scala/com/wordnik/swagger/core/util/TypeUtil.scala
@@ -70,7 +70,10 @@ object TypeUtil {
   def isParameterizedList(gt: Type): Boolean = {
     if (classOf[ParameterizedType].isAssignableFrom(gt.getClass)) {
       val tp = gt.asInstanceOf[ParameterizedType].getRawType
-      (tp == classOf[java.util.List[_]] || tp == classOf[scala.List[_]] || tp == classOf[Seq[_]])
+      (tp == classOf[java.util.List[_]] ||
+        tp == classOf[scala.List[_]] ||
+        tp == classOf[java.util.Collection[_]] ||
+        tp == classOf[Seq[_]])
     }
     else false
   }
