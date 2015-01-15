56a224ed6188e6b4bb39c73b513f0709981d6d3f
Martin Ehrnhoefer <martin@ehrnhoefer.com>
2015-01-15 10:08:58 +0100

    Flickering ModelConverterTest fixed
    Don't rely on ordering in a HashSet

--- modules/swagger-core/src/test/scala/models/ModelPropertyName.java
+++ modules/swagger-core/src/test/scala/models/ModelPropertyName.java
@@ -1,8 +1,5 @@
 package models;
 
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
-@JsonPropertyOrder({ "is_persistent", "gettersAndHaters" })
 public class ModelPropertyName {
   public boolean is_persistent() { return true; }
   public String gettersAndHaters() { return null; }
