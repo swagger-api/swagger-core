d1a8e656ceb2eeff1fd1180f2af2aa46977cffdc
Tony Tam <fehguy@gmail.com>
2015-01-29 17:38:31 -0800

    updated interface

--- modules/swagger-jersey-jaxrs/src/main/java/com/wordnik/swagger/jersey/SwaggerJerseyJaxrs.java
+++ modules/swagger-jersey-jaxrs/src/main/java/com/wordnik/swagger/jersey/SwaggerJerseyJaxrs.java
@@ -8,24 +8,32 @@ import com.wordnik.swagger.models.properties.*;
 import com.sun.jersey.multipart.FormDataParam;
 import com.sun.jersey.api.core.InjectParam;
 
+import java.util.*;
 import java.lang.annotation.Annotation;
 
 public class SwaggerJerseyJaxrs implements SwaggerExtension {
-  public Parameter processParameter(Annotation[] annotations, Class<?> cls, boolean isArray) {
+  public List<Parameter> extractParameters(Annotation[] annotations, Class<?> cls, boolean isArray, Iterator<SwaggerExtension> chain) {
     for(Annotation annotation : annotations) {
       if(annotation instanceof FormDataParam) {
+        List<Parameter> output = new ArrayList<Parameter>();
         FormDataParam fd = (FormDataParam) annotation;
-        if(java.io.InputStream.class.equals(cls))
-          return new FormParameter().type("file").name(fd.value());
+        if(java.io.InputStream.class.equals(cls)) {
+          Parameter param = new FormParameter().type("file").name(fd.value());
+          output.add(param);
+          return output;
+        }
         else {
           FormParameter fp = new FormParameter().name(fd.value());
           Property schema = ModelConverters.getInstance().readAsProperty(cls);
           if(schema != null)
             fp.setProperty(schema);
-          return fp;
+          output.add(fp);
+          return output;
         }
       }
     }
+    if(chain.hasNext())
+      return chain.next().extractParameters(annotations, cls, isArray, chain);
     return null;
   }
 
