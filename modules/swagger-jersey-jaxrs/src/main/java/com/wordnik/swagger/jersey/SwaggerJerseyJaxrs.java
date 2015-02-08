c9bbe7d6d2dd10c33d3a48f5889de365ed8c2221
Tony Tam <fehguy@gmail.com>
2015-02-08 08:53:19 -0800

    updated logic to pass ignored class set to extensions

--- modules/swagger-jersey-jaxrs/src/main/java/com/wordnik/swagger/jersey/SwaggerJerseyJaxrs.java
+++ modules/swagger-jersey-jaxrs/src/main/java/com/wordnik/swagger/jersey/SwaggerJerseyJaxrs.java
@@ -12,10 +12,15 @@ import java.util.*;
 import java.lang.annotation.Annotation;
 
 public class SwaggerJerseyJaxrs implements SwaggerExtension {
-  public List<Parameter> extractParameters(Annotation[] annotations, Class<?> cls, boolean isArray, Iterator<SwaggerExtension> chain) {
+  public List<Parameter> extractParameters(Annotation[] annotations, Class<?> cls, boolean isArray, Set<Class<?>> classesToSkip, Iterator<SwaggerExtension> chain) {
+    List<Parameter> output = new ArrayList<Parameter>();
+    if(shouldIgnoreClass(cls) || classesToSkip.contains(cls)) {
+      // stop the processing chain
+      classesToSkip.add(cls);
+      return output;
+    }
     for(Annotation annotation : annotations) {
       if(annotation instanceof FormDataParam) {
-        List<Parameter> output = new ArrayList<Parameter>();
         FormDataParam fd = (FormDataParam) annotation;
         if(java.io.InputStream.class.equals(cls)) {
           Parameter param = new FormParameter().type("file").name(fd.value());
@@ -33,14 +38,17 @@ public class SwaggerJerseyJaxrs implements SwaggerExtension {
       }
     }
     if(chain.hasNext())
-      return chain.next().extractParameters(annotations, cls, isArray, chain);
+      return chain.next().extractParameters(annotations, cls, isArray, classesToSkip, chain);
     return null;
   }
 
   public boolean shouldIgnoreClass(Class<?> cls) {
+    boolean output = false;
     if(com.sun.jersey.core.header.FormDataContentDisposition.class.equals(cls))
-      return true;
+      output = true;
     else
-      return false;
+      output = false;
+    System.out.println("should ignore " + cls + ": " + output);
+    return output;
   }
 }
