5f3946fcbf082ad53f0b7def05cd250bd1070bc2
Ron <webron@gmail.com>
2015-05-05 13:59:52 +0300

    response code docs

--- modules/swagger-annotations/src/main/java/com/wordnik/swagger/annotations/ApiOperation.java
+++ modules/swagger-annotations/src/main/java/com/wordnik/swagger/annotations/ApiOperation.java
@@ -164,6 +164,10 @@ public @interface ApiOperation {
    */
   ResponseHeader[] responseHeaders() default @ResponseHeader(name = "", response = Void.class);
 
-  /** HTTP status code */
+  /**
+   * The HTTP status code of the response.
+   * <p/>
+   * The value should be one of the formal <a target="_blank" href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html">HTTP Status Code Definitions</a>.
+   */
   int code() default 200;
 }
