280da07152e38589f7d4354600421ad674cfcd5c
Tony Tam <fehguy@gmail.com>
2015-05-19 14:25:32 -0700

    added files

---
+++ modules/swagger-core/src/test/scala/models/composition/DigitalSignal.java
@@ -0,0 +1,16 @@
+package models.composition;
+
+import com.wordnik.swagger.annotations.ApiModel;
+
+@ApiModel(description = "AnalogSignal", parent = DigitalSignal.class)
+public class DigitalSignal extends AbstractSignal implements Signal {
+  private boolean digitalValue;
+
+  public boolean getDigitalValue() {
+    return digitalValue;
+  }
+
+  public void setDigitalValue(boolean digitalValue) {
+    this.digitalValue = digitalValue;
+  }
+}
