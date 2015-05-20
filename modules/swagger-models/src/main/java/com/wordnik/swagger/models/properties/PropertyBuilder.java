71a04d0418bd5d0bf0f528a120c70646dc58d3dc
b_sapir <Boaz_Sapir@intuit.com>
2015-05-20 18:07:47 +0300

    Support binary input and output

--- modules/swagger-models/src/main/java/com/wordnik/swagger/models/properties/PropertyBuilder.java
+++ modules/swagger-models/src/main/java/com/wordnik/swagger/models/properties/PropertyBuilder.java
@@ -3,33 +3,29 @@ package com.wordnik.swagger.models.properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 public class PropertyBuilder {
   static Logger LOGGER = LoggerFactory.getLogger(PropertyBuilder.class);
 
-  public static Property build(String type, String format, Map<String, Object> args) {
+  public static Property build(String type, String format, Map<PropertyId, Object> args) {
     if(args == null) {
-      args = new HashMap<String, Object>();
+      args = Collections.emptyMap();
     }
 
-    List<String> _enum = (List<String>) args.get("enum");
-    String title = (String)args.get("title");
-    String description = (String)args.get("description");
-    String _default = (String)args.get("default");
-    String pattern = (String)args.get("pattern");
-    String discriminator = (String)args.get("discriminator");
-    Integer minItems = (Integer)args.get("minItems");
-    Integer maxItems = (Integer)args.get("maxItems");
-    Integer minProperties = (Integer)args.get("minProperties");
-    Integer maxProperties = (Integer)args.get("maxProperties");
-    Integer minLength = (Integer)args.get("minLength");
-    Integer maxLength = (Integer)args.get("maxLength");
-    Double minimum = (Double)args.get("minimum");
-    Double maximum = (Double)args.get("maximum");
-    Double exclusiveMinimum = (Double)args.get("exclusiveMinimum");
-    Double exclusiveMaximum = (Double)args.get("exclusiveMaximum");
-    Boolean uniqueItems = (Boolean)args.get("uniqueItems");
+    List<String> _enum = PropertyId.ENUM.findValue(args);
+    String title = PropertyId.TITLE.findValue(args);
+    String description = PropertyId.DESCRIPTION.findValue(args);
+    String _default = PropertyId.DEFAULT.findValue(args);
+    String pattern = PropertyId.PATTERN.findValue(args);
+    Integer minLength = PropertyId.MIN_LENGTH.findValue(args);
+    Integer maxLength = PropertyId.MAX_LENGTH.findValue(args);
+    Double minimum = PropertyId.MINIMUM.findValue(args);
+    Double maximum = PropertyId.MAXIMUM.findValue(args);
+    Boolean exclusiveMinimum = PropertyId.EXCLUSIVE_MINIMUM.findValue(args);
+    Boolean exclusiveMaximum = PropertyId.EXCLUSIVE_MAXIMUM.findValue(args);
 
     AbstractProperty property = null;
     if(BooleanProperty.isType(type, format)) {
@@ -46,7 +42,7 @@ public class PropertyBuilder {
         .minimum(minimum)
         .maximum(maximum)
         .exclusiveMinimum(exclusiveMinimum)
-        .exclusiveMaximum(exclusiveMinimum);
+        .exclusiveMaximum(exclusiveMaximum);
     }
     if(FloatProperty.isType(type, format)) {
       property = new FloatProperty()
@@ -54,7 +50,7 @@ public class PropertyBuilder {
         .minimum(minimum)
         .maximum(maximum)
         .exclusiveMinimum(exclusiveMinimum)
-        .exclusiveMaximum(exclusiveMinimum);
+        .exclusiveMaximum(exclusiveMaximum);
     }
     if(FileProperty.isType(type, format)) {
       property = new FileProperty();
@@ -64,14 +60,14 @@ public class PropertyBuilder {
         .minimum(minimum)
         .maximum(maximum)
         .exclusiveMinimum(exclusiveMinimum)
-        .exclusiveMaximum(exclusiveMinimum);
+        .exclusiveMaximum(exclusiveMaximum);
     if(IntegerProperty.isType(type, format)) {
       property = new IntegerProperty()
         ._default(_default)
         .minimum(minimum)
         .maximum(maximum)
         .exclusiveMinimum(exclusiveMinimum)
-        .exclusiveMaximum(exclusiveMinimum);
+        .exclusiveMaximum(exclusiveMaximum);
     }
     if(LongProperty.isType(type, format)) {
       property = new LongProperty()
@@ -79,7 +75,7 @@ public class PropertyBuilder {
         .minimum(minimum)
         .maximum(maximum)
         .exclusiveMinimum(exclusiveMinimum)
-        .exclusiveMaximum(exclusiveMinimum);
+        .exclusiveMaximum(exclusiveMaximum);
     }
     if(RefProperty.isType(type, format))
       property = new RefProperty();
@@ -105,14 +101,14 @@ public class PropertyBuilder {
         .pattern(pattern);
     }
     if(ByteArrayProperty.isType(type, format)){
-      property = new ByteArrayProperty();
+        property = new ByteArrayProperty();
     }
     // general properties
     if(property != null) {
       property
         .title(title)
         .description(description);
-      String example = (String)args.get("example");
+      String example = PropertyId.EXAMPLE.findValue (args);
       if (example != null) {
         property.setExample(example);
       }
@@ -126,9 +122,9 @@ public class PropertyBuilder {
         .minimum(minimum)
         .maximum(maximum)
         .exclusiveMinimum(exclusiveMinimum)
-        .exclusiveMaximum(exclusiveMinimum);
+        .exclusiveMaximum(exclusiveMaximum);
     }
-    if("object".equals(type) && format == null) {
+    if(ObjectProperty.isType(type) && format == null) {
         // fall back to Map if type is object and format is missing
       LOGGER.debug("no format specified for object type, falling back to object");
       property = new ObjectProperty();
@@ -136,5 +132,42 @@ public class PropertyBuilder {
     if(property == null)
       LOGGER.debug("no property for " + type + ", " + format);
     return property;
+  }
+
+  public enum PropertyId {
+    ENUM("enum"),
+    TITLE("title"),
+    DESCRIPTION("description"),
+    DEFAULT("default"),
+    PATTERN("pattern"),
+    DESCRIMINATOR("discriminator"),
+    MIN_ITEMS("minItems"),
+    MAX_ITEMS("maxItems"),
+    MIN_PROPERTIES("minProperties"),
+    MAX_PROPERTIES("maxProperties"),
+    MIN_LENGTH("minLength"),
+    MAX_LENGTH("maxLength"),
+    MINIMUM("minimum"),
+    MAXIMUM("maximum"),
+    EXCLUSIVE_MINIMUM("exclusiveMinimum"),
+    EXCLUSIVE_MAXIMUM("exclusiveMaximum"),
+    UNIQUE_ITEMS("uniqueItems"),
+    EXAMPLE("example"),
+    TYPE("type"),
+    FORMAT("format");
+
+    private String propertyName;
+
+    private PropertyId(String propertyName) {
+      this.propertyName = propertyName;
+    }
+
+    public String getPropertyName() {
+      return propertyName;
+    }
+
+    public <T> T findValue(Map<PropertyId, Object> args) {
+      return (T) args.get(this);
+    }
   }
 }
