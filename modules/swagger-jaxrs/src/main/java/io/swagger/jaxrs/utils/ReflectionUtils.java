f651a6da7029a0d45b4352b804abf8b80ba9d5c1
Ole Lensmar <ole.lensmar@smartbear.com>
2015-06-05 17:58:41 -0400

    Merge branch 'issue-1085' of https://github.com/iushankin/swagger-core into merging-issue-1085
    Conflicts:
    	modules/swagger-jaxrs/src/main/java/io/swagger/jaxrs/Reader.java
    	modules/swagger-jaxrs/src/test/scala/ReaderTest.scala

--- modules/swagger-jaxrs/src/main/java/io/swagger/jaxrs/utils/ReflectionUtils.java
+++ modules/swagger-jaxrs/src/main/java/io/swagger/jaxrs/utils/ReflectionUtils.java
@@ -1,10 +1,34 @@
 package io.swagger.jaxrs.utils;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.Type;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
 
 public class ReflectionUtils {
+
+  private static final Set<Class<? extends Annotation>> CONSTRUCTOR_ANNOTATIONS;
+
+  static {
+    final Set<Class<? extends Annotation>> constructorAnnotations = new HashSet<Class<? extends Annotation>>();
+    constructorAnnotations.add(PathParam.class);
+    constructorAnnotations.add(QueryParam.class);
+    constructorAnnotations.add(HeaderParam.class);
+    CONSTRUCTOR_ANNOTATIONS = Collections.unmodifiableSet(constructorAnnotations);
+  }
 
   /**
    * Checks if the method methodToFind is the overridden method from the superclass.
@@ -82,5 +106,81 @@ public class ReflectionUtils {
       return method;
     }
     return null;
+  }
+
+  /**
+   * Searches for constructor suitable for resource instantiation.
+   * <p/>
+   * If more constructors exists the one with the most injectable parameters will be selected.
+   *
+   * @param cls is the class where to search
+   * @return the suitable constructor
+   */
+  public static Constructor<?> findConstructor(Class<?> cls) {
+    if (cls.isLocalClass() || (cls.isMemberClass() && !Modifier.isStatic(cls.getModifiers()))) {
+      return null;
+    }
+
+    Constructor<?> selected = null;
+    int selectedCount = 0;
+    int maxParams = -1;
+    for (Constructor<?> constructor : cls.getDeclaredConstructors()) {
+      final Class<?>[] parameterTypes = constructor.getParameterTypes();
+      if (parameterTypes.length >= maxParams && isCompatible(constructor)) {
+        if (parameterTypes.length > maxParams) {
+          maxParams = parameterTypes.length;
+          selectedCount = 0;
+        }
+
+        selected = constructor;
+        selectedCount++;
+      }
+    }
+
+    return selectedCount == 1 ? selected : null;
+  }
+
+  /**
+   * Returns an implementation of {@link Function} for getting annotation types.
+   * @return the implementation of {@link Function} for getting annotation types
+   */
+  public static Function<Annotation, Class<? extends Annotation>> createAnnotationTypeGetter() {
+    return new Function<Annotation, Class<? extends Annotation>>() {
+      @Override
+      public Class<? extends Annotation> apply(Annotation annotation) {
+        return annotation.annotationType();
+      }
+    };
+  }
+
+  /**
+   * Checks if the passed constructor is suitable for resource instantiation.
+   * Repeats the logic of the {@link org.glassfish.jersey.internal.inject.JerseyClassAnalyzer#isCompatible(java.lang.reflect.Constructor)}
+   * @param constructor the constructor to be checked
+   * @return true if the constructor is suitable or false otherwise
+   */
+  private static boolean isCompatible(Constructor<?> constructor) {
+    for (Annotation annotation : constructor.getAnnotations()) {
+      // use string name to avoid additional dependencies
+      if ("javax.inject.Inject".equals(annotation.annotationType().getName())) {
+        return true;
+      }
+    }
+
+    if (!Modifier.isPublic(constructor.getModifiers())) {
+      final int access = Modifier.PUBLIC | Modifier.PROTECTED | Modifier.PRIVATE;
+      return constructor.getParameterTypes().length == 0 &&
+        (constructor.getDeclaringClass().getModifiers() & access) == constructor.getModifiers();
+    }
+
+    for (Annotation[] paramAnnotations : constructor.getParameterAnnotations()) {
+      final Collection<Class<? extends Annotation>> tmp = Collections2.transform(Arrays.asList(paramAnnotations),
+        ReflectionUtils.createAnnotationTypeGetter());
+      if (Collections.disjoint(tmp, CONSTRUCTOR_ANNOTATIONS)) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
--- modules/swagger-jaxrs/src/test/scala/ReaderTest.scala
+++ modules/swagger-jaxrs/src/test/scala/ReaderTest.scala
@@ -81,6 +81,24 @@ class ReaderTest extends FlatSpec with Matchers {
     swagger.getPaths().get("/{id}/value").getPut.getProduces.get(0) should equal ("text/plain")
   }
 
+  it should "scan class level and field level annotations" in {
+    val swagger = new Reader(new Swagger()).read(classOf[ResourceWithKnownInjections])
+
+    val resourceParameters = swagger.getPaths().get("/resource/{id}").getGet().getParameters()
+    resourceParameters should not equal (null)
+    resourceParameters.size() should equal (3)
+    resourceParameters.get(0).asInstanceOf[PathParameter].getName() should equal ("id")
+    resourceParameters.get(1).asInstanceOf[QueryParameter].getName() should equal ("fieldParam")
+    resourceParameters.get(2).asInstanceOf[QueryParameter].getName() should equal ("methodParam")
+
+    val subResourceParameters = swagger.getPaths().get("/resource/{id}/subresource").getGet().getParameters()
+    subResourceParameters should not equal (null)
+    subResourceParameters.size() should equal (3)
+    subResourceParameters.get(0).asInstanceOf[PathParameter].getName() should equal ("id")
+    subResourceParameters.get(1).asInstanceOf[QueryParameter].getName() should equal ("fieldParam")
+    subResourceParameters.get(2).asInstanceOf[QueryParameter].getName() should equal ("subResourceParam")
+  }
+
   def isValidRestPath(method: Method) = {
     if(Set(Option(method.getAnnotation(classOf[GET])),
       Option(method.getAnnotation(classOf[PUT])),
