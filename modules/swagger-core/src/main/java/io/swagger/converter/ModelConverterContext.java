27c346fd567e4a578fb88d203ff3ba98ab0631db
unknown <simon00t@WKLCNU409DRYN.intra.kluwer.nl>
2015-06-11 08:59:05 +0200

    Properly resolve a inner model with a self reference.

--- modules/swagger-core/src/main/java/io/swagger/converter/ModelConverterContext.java
+++ modules/swagger-core/src/main/java/io/swagger/converter/ModelConverterContext.java
@@ -19,6 +19,16 @@ public interface ModelConverterContext {
     public void defineModel(String name, Model model);
 
     /**
+     * needs to be called whenever a Model is defined which can be referenced from another
+     * Model or Property
+     *
+     * @param name  the name of the model
+     * @param model the Model
+     * @param type the Type
+     */
+    public void defineModel(String name, Model model, Type type);
+
+    /**
      * @param type the property Class
      * @return a property representation of the Class. Any referenced models will be defined already.
      */
