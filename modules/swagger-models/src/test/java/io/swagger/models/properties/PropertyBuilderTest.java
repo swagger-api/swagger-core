9df1eeefbe05a36a8a96a25b1485b08db28d4f51
Paul Ebermann <paul.ebermann@zalando.de>
2015-08-26 09:53:54 +0200

    Issue #993: add some tests which demonstrate the problem.
    Also, split the previous test into two and remove some superfluous
    imports from the same package, and add tests for number properties.

--- modules/swagger-models/src/test/java/io/swagger/models/properties/PropertyBuilderTest.java
+++ modules/swagger-models/src/test/java/io/swagger/models/properties/PropertyBuilderTest.java
@@ -1,26 +1,59 @@
 package io.swagger.models.properties;
 
-import io.swagger.models.properties.Property;
-
 import org.testng.Assert;
+
 import org.testng.annotations.Test;
-
-import io.swagger.models.properties.StringProperty;
-import io.swagger.models.properties.PropertyBuilder;
-
 
 public class PropertyBuilderTest {
 
     @Test
-    public void testStringFormat() {
-        for (StringProperty.Format format : StringProperty.Format.values()) {
+    public void testStringPredefinedFormats() {
+        for (final StringProperty.Format format : StringProperty.Format.values()) {
             final String name = format.getName();
             final Property property = PropertyBuilder.build(StringProperty.TYPE, name, null);
             Assert.assertEquals(property.getType(), StringProperty.TYPE);
             Assert.assertEquals(property.getFormat(), name);
         }
+    }
+
+    @Test
+    public void testStringNoFormat() {
         final Property noFormat = PropertyBuilder.build(StringProperty.TYPE, null, null);
         Assert.assertEquals(noFormat.getType(), StringProperty.TYPE);
         Assert.assertNull(noFormat.getFormat());
     }
+
+    @Test
+    public void testStringCustomFormat() {
+        final String customFormatName = "custom";
+        final Property customFormatProperty = PropertyBuilder.build(StringProperty.TYPE, customFormatName, null);
+        Assert.assertNotNull(customFormatProperty);
+        Assert.assertEquals(customFormatProperty.getType(), StringProperty.TYPE);
+        Assert.assertEquals(customFormatProperty.getFormat(), customFormatName);
+    }
+
+    @Test
+    public void testNumberFloat() {
+        final String floatFormat = "float";
+        final Property floatProperty = PropertyBuilder.build(DecimalProperty.TYPE, floatFormat, null);
+        Assert.assertEquals(floatProperty.getType(), DecimalProperty.TYPE);
+        Assert.assertEquals(floatProperty.getFormat(), floatFormat);
+    }
+
+    @Test
+    public void testNumberNoFormat() {
+        final Property noFormat = PropertyBuilder.build(DecimalProperty.TYPE, null, null);
+        Assert.assertEquals(noFormat.getType(), DecimalProperty.TYPE);
+        Assert.assertNull(noFormat.getFormat());
+    }
+
+    @Test
+    public void testNumberCustomFormat() {
+        final String customFormatName = "custom";
+        final Property customFormatProperty = PropertyBuilder.build(DecimalProperty.TYPE, customFormatName, null);
+        Assert.assertNotNull(customFormatProperty);
+        Assert.assertEquals(customFormatProperty.getType(), DecimalProperty.TYPE);
+        Assert.assertEquals(customFormatProperty.getFormat(), customFormatName);
+    }
+
 }
