2b88e4a891fadbe2df124d2d2e12d060aeadd938
Aki Yoshida <elakito@gmail.com>
2015-09-03 17:55:48 +0200

    #1244 use reflection to support BeanParam

--- modules/swagger-jersey2-jaxrs/src/main/java/io/swagger/jersey/SwaggerJersey2Jaxrs.java
+++ modules/swagger-jersey2-jaxrs/src/main/java/io/swagger/jersey/SwaggerJersey2Jaxrs.java
@@ -41,55 +41,8 @@ public class SwaggerJersey2Jaxrs extends AbstractSwaggerExtension {
             return parameters;
         }
         for (final Annotation annotation : annotations) {
-            if (annotation instanceof BeanParam) {
-                // Use Jackson's logic for processing Beans
-                final BeanDescription beanDesc = mapper.getSerializationConfig().introspect(constructType(type));
-                final List<BeanPropertyDefinition> properties = beanDesc.findProperties();
-
-                for (final BeanPropertyDefinition propDef : properties) {
-                    final AnnotatedField field = propDef.getField();
-                    final AnnotatedMethod setter = propDef.getSetter();
-                    final List<Annotation> paramAnnotations = new ArrayList<Annotation>();
-                    final Iterator<SwaggerExtension> extensions = SwaggerExtensions.chain();
-                    Type paramType = null;
-
-                    // Gather the field's details
-                    if (field != null) {
-                        paramType = field.getGenericType();
-
-                        for (final Annotation fieldAnnotation : field.annotations()) {
-                            if (!paramAnnotations.contains(fieldAnnotation)) {
-                                paramAnnotations.add(fieldAnnotation);
-                            }
-                        }
-                    }
-
-                    // Gather the setter's details but only the ones we need
-                    if (setter != null) {
-                        // Do not set the param class/type from the setter if the values are already identified
-                        if (paramType == null) {
-                            paramType = setter.getGenericParameterTypes() != null ? setter.getGenericParameterTypes()[0] : null;
-                        }
-
-                        for (final Annotation fieldAnnotation : setter.annotations()) {
-                            if (!paramAnnotations.contains(fieldAnnotation)) {
-                                paramAnnotations.add(fieldAnnotation);
-                            }
-                        }
-                    }
-
-                    // Re-process all Bean fields and let the default swagger-jaxrs/swagger-jersey-jaxrs processors do their thing
-                    List<Parameter> extracted =
-                            extensions.next().extractParameters(paramAnnotations, paramType, typesToSkip, extensions);
-
-                    // since downstream processors won't know how to introspect @BeanParam, process here
-                    for (Parameter param : extracted) {
-                        if (ParameterProcessor.applyAnnotations(null, param, paramType, paramAnnotations) != null) {
-                            parameters.add(param);
-                        }
-                    }
-                }
-            } else if (annotation instanceof FormDataParam) {
+            // just handle the jersey specific annotation
+            if (annotation instanceof FormDataParam) {
                 FormDataParam fd = (FormDataParam) annotation;
                 if (java.io.InputStream.class.isAssignableFrom(constructType(type).getRawClass())) {
                     final Parameter param = new FormParameter().type("file").name(fd.value());
