3afa0b276679418f4ddd966136d0aedf6e130665
Tomasz Wysocki <t.wysocki@oberthur.com>
2015-09-05 22:43:15 +0200

    Add support for javax.validation.Pattern

--- modules/swagger-core/src/test/java/io/swagger/models/BeanValidationsModel.java
+++ modules/swagger-core/src/test/java/io/swagger/models/BeanValidationsModel.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class BeanValidationsModel {
@@ -21,6 +22,8 @@ public class BeanValidationsModel {
     protected String password;
 
     protected String passwordHint;
+
+    @Pattern(regexp = "(.+?)@(.+?)")
     protected String email;
 
     @DecimalMin(value = "0.1", inclusive = false)
