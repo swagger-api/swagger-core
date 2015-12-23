8921f2f566aa8a1fd3917610fd3c0802017bed2a
Aki Yoshida <elakito@gmail.com>
2015-12-23 11:34:23 +0100

    fix #1574 allow visit the same sub resources along different paths

--- modules/swagger-jaxrs/src/test/java/io/swagger/resources/SimpleSelfReferencingSubResource.java
+++ modules/swagger-jaxrs/src/test/java/io/swagger/resources/SimpleSelfReferencingSubResource.java
@@ -29,6 +29,10 @@ public class SimpleSelfReferencingSubResource {
         public SubResource2 retrieveSelf2() {
             return new SubResource2();
         }
+        @Path("/leaf")
+        public SubResource3 retrieveLeaf() {
+            return new SubResource3();
+        }
     }
 
     public static class SubResource2 {
@@ -46,6 +50,18 @@ public class SimpleSelfReferencingSubResource {
         @Path("/recurse1")
         public SubResource retrieveSelf1() {
             return new SubResource();
+        }
+        @Path("/leaf")
+        public SubResource3 retrieveLeaf() {
+            return new SubResource3();
+        }
+    }
+
+    public static class SubResource3 {
+        @GET
+        @Produces("application/json")
+        public SubResource3 retrieve() {
+            return this;
         }
     }
 }
