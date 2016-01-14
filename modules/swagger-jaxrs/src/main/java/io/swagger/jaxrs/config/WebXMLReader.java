4c815c5ab791c101165ee04c2242a4470f79635c
Tony Tam <fehguy@gmail.com>
2016-01-14 14:25:58 -0800

    added host, schemes, proper handling of `basePath` #1583

--- modules/swagger-jaxrs/src/main/java/io/swagger/jaxrs/config/WebXMLReader.java
+++ modules/swagger-jaxrs/src/main/java/io/swagger/jaxrs/config/WebXMLReader.java
@@ -12,9 +12,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletConfig;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WebXMLReader implements SwaggerConfig {
-    protected String basePath, host, filterClass, apiVersion, title, scheme = "http";
+    protected String basePath,
+            host,
+            filterClass,
+            apiVersion,
+            title;
+    protected String[] schemes = new String[]{"http"};
     private Logger LOGGER = LoggerFactory.getLogger(WebXMLReader.class);
 
     public WebXMLReader(ServletConfig servletConfig) {
@@ -29,25 +36,46 @@ public class WebXMLReader implements SwaggerConfig {
         if (shouldPrettyPrint != null) {
             scanner.setPrettyPrint(Boolean.parseBoolean(shouldPrettyPrint));
         }
-        basePath = servletConfig.getInitParameter("swagger.api.basepath");
-        title = servletConfig.getInitParameter("swagger.api.title");
+
+        // we support full base path (i.e full URL to the server) or just base path
+        this.host = servletConfig.getInitParameter("swagger.api.host");
+        String schemesString = servletConfig.getInitParameter("swagger.api.schemes");
+
+        // split the CSV string and update the `schemes` variable
+        if(schemesString != null) {
+            String[] parts = schemesString.split(",");
+            List<String> schemes = new ArrayList<String>();
+            for(String scheme : parts) {
+                String s = scheme.trim();
+                if(!s.isEmpty()) {
+                    schemes.add(s);
+                }
+            }
+            this.schemes = schemes.toArray(new String[schemes.size()]);
+        }
+        this.title = servletConfig.getInitParameter("swagger.api.title");
+
         if (title == null) {
             title = "";
         }
 
+        this.basePath = servletConfig.getInitParameter("swagger.api.basepath");
         if (basePath != null) {
             String[] parts = basePath.split("://");
             if (parts.length > 1) {
                 int pos = parts[1].indexOf("/");
                 if (pos >= 0) {
-                    scheme = parts[0];
+                    this.schemes = new String[]{parts[0]};
                     basePath = parts[1].substring(pos);
                     host = parts[1].substring(0, pos);
                 } else {
-                    scheme = parts[0];
+                    this.schemes = new String[]{parts[0]};
                     basePath = null;
                     host = parts[1];
                 }
+            }
+            else {
+                // it is a proper basePath, nothing to do
             }
         }
 
@@ -75,9 +103,13 @@ public class WebXMLReader implements SwaggerConfig {
                 swagger.info(new Info());
             }
 
+            List<Scheme> schemes = new ArrayList<Scheme>();
+            for(String scheme : this.schemes) {
+                schemes.add(Scheme.forValue(scheme));
+            }
             swagger.basePath(basePath)
                     .host(host)
-                    .scheme(Scheme.forValue(scheme))
+                    .schemes(schemes)
                     .getInfo()
                     .title(title)
                     .version(apiVersion);
