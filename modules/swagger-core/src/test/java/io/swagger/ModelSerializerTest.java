3f1c673602646cd2bb1fc7af3b47ec12d7708112
Tony Tam <fehguy@gmail.com>
2016-12-26 23:39:25 -0800

    added uniqueItems

--- modules/swagger-core/src/test/java/io/swagger/ModelSerializerTest.java
+++ modules/swagger-core/src/test/java/io/swagger/ModelSerializerTest.java
@@ -27,6 +27,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertTrue;
@@ -312,5 +313,29 @@ public class ModelSerializerTest {
         assertEquals(model.getMinimum().intValue(), 10);
         assertEquals(model.getMaximum().intValue(), 20);
         assertEquals(model.getDefaultValue(), 15);
+    }
+
+    @Test
+    public void testIssue2064Neg() throws Exception {
+        String json = "{\n" +
+            "  \"type\": \"string\",\n" +
+            "  \"uniqueItems\": false\n" +
+            "}";
+
+        final ModelImpl model = Json.mapper().readValue(json, ModelImpl.class);
+
+        assertTrue(model.getUniqueItems());
+    }
+
+    @Test
+    public void testIssue2064() throws Exception {
+        String json = "{\n" +
+            "  \"type\": \"string\",\n" +
+            "  \"uniqueItems\": true\n" +
+            "}";
+
+        final ModelImpl model = Json.mapper().readValue(json, ModelImpl.class);
+
+        assertFalse(model.getUniqueItems());
     }
 }
