652e708c1a4cd42fc0701b420d003f46beb48c13
Manuel Boillod <boillodmanuel@gmail.com>
2017-04-25 09:41:22 +0200

    add unit test showing deserialization issue of number type without format (#2178)

--- modules/swagger-core/src/test/java/io/swagger/ObjectPropertyTest.java
+++ modules/swagger-core/src/test/java/io/swagger/ObjectPropertyTest.java
@@ -6,6 +6,7 @@ import static org.testng.Assert.assertTrue;
 
 import java.io.IOException;
 
+import io.swagger.models.properties.DoubleProperty;
 import org.testng.annotations.Test;
 import io.swagger.models.ModelImpl;
 import io.swagger.models.properties.ObjectProperty;
@@ -47,4 +48,46 @@ public class ObjectPropertyTest {
         assertNotNull(op.getVendorExtensions().get("x-foo"));
         assertEquals(op.getVendorExtensions().get("x-foo"), "vendor x");
     }
+
+    @Test(description = "it should deserialize a property with enum and default")
+    public void deserializeNumberType() throws IOException {
+        final String yamlNumberWithFormat = "---\n" +
+                "type: \"object\"\n" +
+                "properties:\n" +
+                "  id:\n" +
+                "    type: \"number\"\n" +
+                "    format: \"double\"\n" +
+                "    enum: [1.0, 2.0]\n" +
+                "    default: 1.0";
+
+        final ModelImpl modelWithFormat = io.swagger.util.Yaml.mapper().readValue(yamlNumberWithFormat, ModelImpl.class);
+
+        final DoubleProperty propertyWithFormat = (DoubleProperty)modelWithFormat.getProperties().get("id");
+        assertNotNull(propertyWithFormat);
+        assertNotNull(propertyWithFormat.getEnum());
+        assertEquals(propertyWithFormat.getEnum().size(), 2);
+        assertEquals(propertyWithFormat.getEnum().get(0), 1.0d);
+        assertEquals(propertyWithFormat.getEnum().get(1), 2.0d);
+        assertEquals(propertyWithFormat.getDefault(), 1.0d);
+
+        final String yamlNumberWithoutFormat = "---\n" +
+                "type: \"object\"\n" +
+                "properties:\n" +
+                "  id:\n" +
+                "    type: \"number\"\n" +
+                "    enum: [1.0, 2.0]\n" +
+                "    default: 1.0";
+
+        final ModelImpl modelWithoutFormat = io.swagger.util.Yaml.mapper().readValue(yamlNumberWithoutFormat, ModelImpl.class);
+
+        // NOTE: Expect to be a DoubleProperty because DecimalProperty didn't have enum or default attributes
+        final DoubleProperty propertyWithoutFormat = (DoubleProperty)modelWithoutFormat.getProperties().get("id");
+        assertNotNull(propertyWithoutFormat);
+        assertNotNull(propertyWithoutFormat.getEnum());
+        assertEquals(propertyWithoutFormat.getEnum().size(), 2);
+        assertEquals(propertyWithoutFormat.getEnum().get(0), 1.0d);
+        assertEquals(propertyWithoutFormat.getEnum().get(1), 2.0d);
+        assertEquals(propertyWithoutFormat.getDefault(), 1.0d);
+    }
+
 }
