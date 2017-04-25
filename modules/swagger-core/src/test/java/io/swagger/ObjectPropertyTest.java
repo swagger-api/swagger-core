d3d951533ac5eba106a90fbfbd0acadf4d147a8b
Manuel Boillod <boillodmanuel@gmail.com>
2017-04-25 09:48:55 +0200

    add unit test showing deserialization issue of number type without format (#2178)

--- modules/swagger-core/src/test/java/io/swagger/ObjectPropertyTest.java
+++ modules/swagger-core/src/test/java/io/swagger/ObjectPropertyTest.java
@@ -1,18 +1,19 @@
 package io.swagger;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
-import static org.testng.Assert.assertTrue;
-
-import java.io.IOException;
-
-import io.swagger.models.properties.DoubleProperty;
-import org.testng.annotations.Test;
 import io.swagger.models.ModelImpl;
+import io.swagger.models.properties.DoubleProperty;
 import io.swagger.models.properties.ObjectProperty;
 import io.swagger.models.properties.Property;
 import io.swagger.models.properties.StringProperty;
 import io.swagger.util.Json;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
 
 public class ObjectPropertyTest {
     @Test (description = "convert a model with object properties")
@@ -58,6 +59,8 @@ public class ObjectPropertyTest {
                 "    type: \"number\"\n" +
                 "    format: \"double\"\n" +
                 "    enum: [1.0, 2.0]\n" +
+                "    minimum: 1.0\n" +
+                "    maximum: 1.0\n" +
                 "    default: 1.0";
 
         final ModelImpl modelWithFormat = io.swagger.util.Yaml.mapper().readValue(yamlNumberWithFormat, ModelImpl.class);
@@ -65,6 +68,8 @@ public class ObjectPropertyTest {
         final DoubleProperty propertyWithFormat = (DoubleProperty)modelWithFormat.getProperties().get("id");
         assertNotNull(propertyWithFormat);
         assertNotNull(propertyWithFormat.getEnum());
+        assertEquals(propertyWithFormat.getMinimum(), new BigDecimal("1.0"));
+        assertEquals(propertyWithFormat.getMaximum(), new BigDecimal("1.0"));
         assertEquals(propertyWithFormat.getEnum().size(), 2);
         assertEquals(propertyWithFormat.getEnum().get(0), 1.0d);
         assertEquals(propertyWithFormat.getEnum().get(1), 2.0d);
@@ -83,6 +88,8 @@ public class ObjectPropertyTest {
         // NOTE: Expect to be a DoubleProperty because DecimalProperty didn't have enum or default attributes
         final DoubleProperty propertyWithoutFormat = (DoubleProperty)modelWithoutFormat.getProperties().get("id");
         assertNotNull(propertyWithoutFormat);
+        assertEquals(propertyWithoutFormat.getMinimum(), new BigDecimal("1.0"));
+        assertEquals(propertyWithoutFormat.getMaximum(), new BigDecimal("1.0"));
         assertNotNull(propertyWithoutFormat.getEnum());
         assertEquals(propertyWithoutFormat.getEnum().size(), 2);
         assertEquals(propertyWithoutFormat.getEnum().get(0), 1.0d);
