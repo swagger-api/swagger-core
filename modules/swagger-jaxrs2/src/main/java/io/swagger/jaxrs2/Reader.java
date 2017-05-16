5f713655885cbeb998d380b06e1649e3a24c1983
Rafael Lopez <RafaelLopez@MacBook-Pro-de-Rafael.local>
2017-05-16 12:51:46 -0500

    Simple ScanMethods Test

--- modules/swagger-jaxrs2/src/main/java/io/swagger/jaxrs2/Reader.java
+++ modules/swagger-jaxrs2/src/main/java/io/swagger/jaxrs2/Reader.java
@@ -1,6 +1,16 @@
 package io.swagger.jaxrs2;
 
+import com.fasterxml.jackson.databind.BeanDescription;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import io.swagger.oas.annotations.responses.ApiResponse;
 import io.swagger.oas.models.OpenAPI;
+import io.swagger.oas.models.Operation;
+import io.swagger.oas.models.parameters.Parameter;
+
+import java.lang.reflect.Method;
+import java.util.Collections;
 
 
 public class Reader {
@@ -11,6 +21,11 @@ public class Reader {
     }
 
 
-
-
+    public Operation parseMethod(Method method) {
+        //JavaType classType = TypeFactory.defaultInstance().constructType(method.getDeclaringClass());
+        //BeanDescription bd = new ObjectMapper().getSerializationConfig().introspect(classType);
+        //return parseMethod(classType.getClass(), method, bd.findMethod(method.getName(), method.getParameterTypes()),
+        //Collections.<Parameter> emptyList(), Collections.<ApiResponse> emptyList());
+        return new Operation();
+    }
 }
--- modules/swagger-jaxrs2/src/test/java/io/swagger/jaxrs2/ReaderTest.java
+++ modules/swagger-jaxrs2/src/test/java/io/swagger/jaxrs2/ReaderTest.java
@@ -1,15 +1,39 @@
 package io.swagger.jaxrs2;
 
+import io.swagger.jaxrs2.resources.SimpleMethods;
 import io.swagger.oas.models.OpenAPI;
+import io.swagger.oas.models.Operation;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
+
+import javax.ws.rs.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 
 public class ReaderTest {
     @Test(description = "scan methods")
     public void scanMethods() {
-
+        Method[] methods = SimpleMethods.class.getMethods();
         Reader reader = new Reader(new OpenAPI());
+        for (final Method method : methods) {
+            if (isValidRestPath(method)) {
+                Operation operation = reader.parseMethod(method);
+                Assert.assertNotNull(operation);
+            }
+        }
 
+    }
+
+    private Boolean isValidRestPath(Method method) {
+        for (Class<? extends Annotation> item : Arrays.asList(GET.class, PUT.class, POST.class, DELETE.class,
+                OPTIONS.class, HEAD.class)) {
+            if (method.getAnnotation(item) != null) {
+                return true;
+            }
+        }
+        return false;
     }
 }
