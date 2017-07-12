b8ac2234f9fb8cc040fd8cd8b75d0db5e39f987d
Navid Shakibapour Tabrizi <navidst@ca.ibm.com>
2017-07-12 15:32:38 -0400

    Extracted OpenAPIConfig interface

--- modules/swagger-web/src/main/java/io/swagger/oas/web/OpenAPIConfig.java
+++ modules/swagger-web/src/main/java/io/swagger/oas/web/OpenAPIConfig.java
@@ -1,91 +1,23 @@
 package io.swagger.oas.web;
 
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import io.swagger.oas.models.OpenAPI;
 
-public final class OpenAPIConfig {
-    private Set<Class<?>> classesToBeScanned;
-    private Map<String, Object> environment;
-    private Class<?> filterClass;
-    private Collection<String> ignoredRoutes;
-    private OpenAPI openAPI;
-    private boolean prettyPrint;
-    private boolean scanAllResources;
+public interface OpenAPIConfig {
+    Set<Class<?>> getClasses();
 
-    public OpenAPIConfig() {
-        classesToBeScanned = Collections.<Class<?>>emptySet();
-        environment = Collections.<String, Object>emptyMap();
-        ignoredRoutes = Collections.<String>emptySet();
-    }
+    Class<?> getFilterClass();
 
-    public Set<Class<?>> getClasses() {
-        return classesToBeScanned;
-    }
+    Collection<String> getIgnoredRoutes();
 
-    public OpenAPIConfig setClasses(Set<Class<?>> classesToBeScanned) {
-        this.classesToBeScanned = classesToBeScanned == null || classesToBeScanned.isEmpty()
-                ? Collections.<Class<?>>emptySet() : Collections.unmodifiableSet(classesToBeScanned);
-        return this;
-    }
+    OpenAPI getOpenAPI();
 
-    public Class<?> getFilterClass() {
-        return filterClass;
-    }
+    Map<String, Object> getUserDefinedOptions();
 
-    public OpenAPIConfig setFilterClass(Class<?> filterClass) {
-        this.filterClass = filterClass;
-        return this;
-    }
+    boolean isPrettyPrint();
 
-    public Collection<String> getIgnoredRoutes() {
-        return ignoredRoutes;
-    }
-
-    public OpenAPIConfig setIgnoredRoutes(Collection<String> ignoredRoutes) {
-        this.ignoredRoutes = ignoredRoutes == null || ignoredRoutes.isEmpty() ? Collections.<String>emptySet()
-                : Collections.unmodifiableCollection(new HashSet<String>(ignoredRoutes));
-        return this;
-    }
-
-    public OpenAPI getOpenAPI() {
-        return openAPI;
-    }
-
-    public OpenAPIConfig setOpenAPI(OpenAPI openAPI) {
-        this.openAPI = openAPI;
-        return this;
-    }
-
-    public Map<String, Object> getUserDefinedOptions() {
-        return environment;
-    }
-
-    public OpenAPIConfig setUserDefinedOptions(Map<String, Object> environment) {
-        this.environment = environment == null || environment.isEmpty() ? Collections.<String, Object>emptyMap()
-                : Collections.unmodifiableMap(new HashMap<>(environment));
-        return this;
-    }
-
-    public boolean isPrettyPrint() {
-        return prettyPrint;
-    }
-
-    public OpenAPIConfig setPrettyPrint(boolean prettyPrint) {
-        this.prettyPrint = prettyPrint;
-        return this;
-    }
-
-    public boolean isScanAllResources() {
-        return scanAllResources;
-    }
-
-    public void setScanAllResources(boolean scanAllResources) {
-        this.scanAllResources = scanAllResources;
-    }
+    boolean isScanAllResources();
 }
