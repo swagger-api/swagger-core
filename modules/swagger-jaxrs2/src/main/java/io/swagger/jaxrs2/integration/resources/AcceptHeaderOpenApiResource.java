5562ec2a9b3ffa8e5d132e14e9c5fc8c370597c1
frantuma <frantuma@yahoo.com>
2017-09-29 18:02:21 +0200

    updates swagger path

--- modules/swagger-jaxrs2/src/main/java/io/swagger/jaxrs2/integration/resources/AcceptHeaderOpenApiResource.java
+++ modules/swagger-jaxrs2/src/main/java/io/swagger/jaxrs2/integration/resources/AcceptHeaderOpenApiResource.java
@@ -11,7 +11,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-@Path("/openApi")
+@Path("/openapi")
 public class AcceptHeaderOpenApiResource extends BaseOpenApiResource {
     @Context
     ServletConfig config;
