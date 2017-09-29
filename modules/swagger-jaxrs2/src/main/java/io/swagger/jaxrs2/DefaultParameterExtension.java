85d8d2fce8253c065ccaa9b02d8a30a58bbea67b
christianc <christian.ciach@energymeteo.de>
2017-09-29 17:41:09 +0200

    #2466 retain generic types for attributes of BeanParam

--- modules/swagger-jaxrs2/src/main/java/io/swagger/jaxrs2/DefaultParameterExtension.java
+++ modules/swagger-jaxrs2/src/main/java/io/swagger/jaxrs2/DefaultParameterExtension.java
@@ -114,7 +114,7 @@ public class DefaultParameterExtension extends AbstractOpenAPIExtension {
 
                 // Gather the field's details
                 if (field != null) {
-                    paramType = field.getRawType();
+                	paramType = field.getType();
 
                     for (final Annotation fieldAnnotation : field.annotations()) {
                         if (!paramAnnotations.contains(fieldAnnotation)) {
@@ -127,7 +127,8 @@ public class DefaultParameterExtension extends AbstractOpenAPIExtension {
                 if (setter != null) {
                     // Do not set the param class/type from the setter if the values are already identified
                     if (paramType == null) {
-                        paramType = setter.getRawParameterTypes() != null ? setter.getRawParameterTypes()[0] : null;
+                    	// paramType will stay null if there is no parameter
+                    	paramType = setter.getParameterType(0); 
                     }
 
                     for (final Annotation fieldAnnotation : setter.annotations()) {
@@ -141,7 +142,7 @@ public class DefaultParameterExtension extends AbstractOpenAPIExtension {
                 if (getter != null) {
                     // Do not set the param class/type from the getter if the values are already identified
                     if (paramType == null) {
-                        paramType = getter.getRawReturnType();
+                        paramType = getter.getType();
                     }
 
                     for (final Annotation fieldAnnotation : getter.annotations()) {
