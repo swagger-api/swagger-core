fe5e8711989df349dfd410ff5157b8a5e3a27030
Nikolay.Slyunkov <nslyunkov@contractor.ea.com>
2017-11-02 19:56:41 +0300

    Parse read only model property flag for all types of properties.

--- modules/swagger-core/src/test/java/io/swagger/properties/PropertyDeserializerTest.java
+++ modules/swagger-core/src/test/java/io/swagger/properties/PropertyDeserializerTest.java
@@ -1,8 +1,11 @@
 package io.swagger.properties;
 
+import io.swagger.models.properties.AbstractProperty;
 import io.swagger.models.properties.IntegerProperty;
 import io.swagger.models.properties.Property;
 import io.swagger.util.Json;
+
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.math.BigDecimal;
@@ -45,5 +48,31 @@ public class PropertyDeserializerTest {
         assertEquals(ip.getMinimum(), new BigDecimal("32"));
         assertEquals(ip.getMaximum(), new BigDecimal("100"));
 
+    }
+
+    @DataProvider
+    public Object[][] readOnlyDataProvider() {
+        return new Object[][] {
+            {"\"type\": \"integer\", \"format\": \"int32\""},
+            {"\"type\": \"string\", \"format\": \"date\""},
+            {"\"type\": \"number\""},
+            {"\"type\": \"array\", \"items\": {\"type\": \"string\"}"},
+            {"\"type\": \"object\", \"additionalProperties\": {\"type\": \"string\"}"},
+            {"\"type\": \"object\", \"properties\": {\"prop1\": {\"type\": \"string\"}}"},
+            {"\"$ref\": \"#/definitions/SomeObject\""}
+        };
+    }
+
+    @Test(dataProvider = "readOnlyDataProvider")
+    public void deserializePropertyWithReadOnlyValue(String type) throws Exception {
+        String json =
+            "{\n" +
+                type + "," +
+                "  \"readOnly\": true" +
+                "}";
+
+        Property param = Json.mapper().readValue(json, Property.class);
+        AbstractProperty property = (AbstractProperty) param;
+        assertTrue(property.getReadOnly());
     }
 }
