014628e1e78bad7f67d67cb3def723c7a5e2bed3
frantuma <frantuma@yahoo.com>
2017-11-09 17:29:28 +0100

    fix example tests

--- modules/swagger-core/src/main/java/io/swagger/v3/core/util/DeserializationModule.java
+++ modules/swagger-core/src/main/java/io/swagger/v3/core/util/DeserializationModule.java
@@ -14,7 +14,6 @@ public class DeserializationModule extends SimpleModule {
 
         this.addDeserializer(Schema.class, new ModelDeserializer());
         this.addDeserializer(Parameter.class, new ParameterDeserializer());
-//        this.addDeserializer(RequestBody.class, new RequestBodyDeserializer());
         this.addDeserializer(Header.StyleEnum.class, new HeaderStyleEnumDeserializer());
         this.addDeserializer(Encoding.StyleEnum.class, new EncodingStyleEnumDeserializer());
         this.addDeserializer(EncodingProperty.StyleEnum.class, new EncodingPropertyStyleEnumDeserializer());
