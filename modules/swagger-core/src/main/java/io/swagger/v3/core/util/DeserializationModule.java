6c9dc30a803ddba459e5d4f4aaf7f78d4806c5c6
frantuma <frantuma@yahoo.com>
2017-11-09 16:06:48 +0100

    remove obsolete deserialization code

--- modules/swagger-core/src/main/java/io/swagger/v3/core/util/DeserializationModule.java
+++ modules/swagger-core/src/main/java/io/swagger/v3/core/util/DeserializationModule.java
@@ -1,26 +1,16 @@
 package io.swagger.v3.core.util;
 
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.headers.Header;
 import io.swagger.v3.oas.models.media.Encoding;
 import io.swagger.v3.oas.models.media.EncodingProperty;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.parameters.Parameter;
-import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 public class DeserializationModule extends SimpleModule {
 
-    public DeserializationModule(boolean includePathDeserializer,
-                                 boolean includeResponseDeserializer) {
-
-        if (includePathDeserializer) {
-            this.addDeserializer(PathItem.class, new PathDeserializer());
-        }
-        if (includeResponseDeserializer) {
-            this.addDeserializer(ApiResponse.class, new ResponseDeserializer());
-        }
+    public DeserializationModule() {
 
         this.addDeserializer(Schema.class, new ModelDeserializer());
         this.addDeserializer(Parameter.class, new ParameterDeserializer());
@@ -31,9 +21,5 @@ public class DeserializationModule extends SimpleModule {
 
 
         this.addDeserializer(SecurityScheme.class, new SecuritySchemeDeserializer());
-    }
-
-    public DeserializationModule() {
-        this(true, true);
     }
 }
