1e4b20ff7f2734583e9e0d4d502384abcb9b1059
christianc <christian.ciach@energymeteo.de>
2017-11-20 14:56:32 +0100

    #2466 cleanup
    Sorry, I didn't know that TestNG expects the "expected value" as the
    second parameter of assertEquals()

--- modules/swagger-jaxrs/src/test/java/io/swagger/BeanParamTest.java
+++ modules/swagger-jaxrs/src/test/java/io/swagger/BeanParamTest.java
@@ -16,8 +16,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 import io.swagger.annotations.Api;
 import io.swagger.jaxrs.DefaultParameterExtension;
 import io.swagger.jaxrs.Reader;
@@ -29,58 +27,56 @@ import io.swagger.models.parameters.Parameter;
 import io.swagger.models.parameters.QueryParameter;
 import io.swagger.models.properties.Property;
 import io.swagger.models.properties.StringProperty;
-import io.swagger.util.Yaml;
 
 public class BeanParamTest {
 
-    @Target({ ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD })
-    @Retention(RetentionPolicy.RUNTIME)
-    public @interface MyBeanParam {
-        // Remove this Annotation and use BeanParam directly with Swagger Core 2.0
-    }
+	@Target({ ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD })
+	@Retention(RetentionPolicy.RUNTIME)
+	public @interface MyBeanParam {
+		// Remove this Annotation and use BeanParam directly with Swagger Core 2.0
+	}
 
-    @Api
-    @Path("/")
-    private static class MyBeanParamResource {
-        @GET
-        public String getWithBeanParam(@MyBeanParam ListOfStringsBeanParam listOfStringsBean) {
-            return "result";
-        }
-    }
+	@Api
+	@Path("/")
+	private static class MyBeanParamResource {
+		@GET
+		public String getWithBeanParam(@MyBeanParam ListOfStringsBeanParam listOfStringsBean) {
+			return "result";
+		}
+	}
 
-    private final SwaggerExtension myDefaultParameterExtension = new DefaultParameterExtension() {
-        @Override
-        protected boolean shouldHandleAdditionalAnnotation(Annotation annotation) {
-            return annotation instanceof MyBeanParam;
-        }
-    };
+	private final SwaggerExtension myDefaultParameterExtension = new DefaultParameterExtension() {
+		@Override
+		protected boolean shouldHandleAdditionalAnnotation(Annotation annotation) {
+			return annotation instanceof MyBeanParam;
+		}
+	};
 
-    private List<SwaggerExtension> originalExtensions;
+	private List<SwaggerExtension> originalExtensions;
 
-    @BeforeMethod
-    public void beforeMethod() {
-        originalExtensions = SwaggerExtensions.getExtensions();
-        SwaggerExtensions.setExtensions(Collections.singletonList(myDefaultParameterExtension));
-    }
+	@BeforeMethod
+	public void beforeMethod() {
+		originalExtensions = SwaggerExtensions.getExtensions();
+		SwaggerExtensions.setExtensions(Collections.singletonList(myDefaultParameterExtension));
+	}
 
-    @AfterMethod
-    public void afterMethod() {
-        SwaggerExtensions.setExtensions(originalExtensions);
-    }
+	@AfterMethod
+	public void afterMethod() {
+		SwaggerExtensions.setExtensions(originalExtensions);
+	}
 
-    @Test(description = "check array type of serialized BeanParam containing QueryParams") // tests issue #2466
-    public void shouldSerializeTypeParameter() throws JsonProcessingException {
-        Swagger swagger = new Reader(new Swagger()).read(MyBeanParamResource.class);
-        System.out.println(Yaml.mapper().writeValueAsString(swagger));
-        List<Parameter> getOperationParams = swagger.getPath("/").getGet().getParameters();
-        Assert.assertEquals(1, getOperationParams.size());
-        QueryParameter param = (QueryParameter) getOperationParams.get(0);
-        Assert.assertEquals("listOfStrings", param.getName());
-        Assert.assertEquals("array", param.getType());
-        // These are the important checks:
-        Property itemsProperty = param.getItems();
-        Assert.assertTrue(itemsProperty instanceof StringProperty);
-        Assert.assertEquals("string", itemsProperty.getType());
-    }
+	@Test(description = "check array type of serialized BeanParam containing QueryParams") // tests issue #2466
+	public void shouldSerializeTypeParameter() {
+		Swagger swagger = new Reader(new Swagger()).read(MyBeanParamResource.class);
+		List<Parameter> getOperationParams = swagger.getPath("/").getGet().getParameters();
+		Assert.assertEquals(getOperationParams.size(), 1);
+		QueryParameter param = (QueryParameter) getOperationParams.get(0);
+		Assert.assertEquals(param.getName(), "listOfStrings");
+		Assert.assertEquals(param.getType(), "array");
+		// These are the important checks:
+		Property itemsProperty = param.getItems();
+		Assert.assertEquals(itemsProperty.getClass(), StringProperty.class);
+		Assert.assertEquals(itemsProperty.getType(), "string");
+	}
 
 }
