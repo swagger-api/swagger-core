e4f7cfee29ab197adab825359098cb2566b1e463
christianc <christian.ciach@energymeteo.de>
2017-11-20 15:08:30 +0100

    #2466 use correct formatting...

--- modules/swagger-jaxrs/src/test/java/io/swagger/BeanParamTest.java
+++ modules/swagger-jaxrs/src/test/java/io/swagger/BeanParamTest.java
@@ -30,53 +30,53 @@ import io.swagger.models.properties.StringProperty;
 
 public class BeanParamTest {
 
-	@Target({ ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD })
-	@Retention(RetentionPolicy.RUNTIME)
-	public @interface MyBeanParam {
-		// Remove this Annotation and use BeanParam directly with Swagger Core 2.0
-	}
+    @Target({ ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD })
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface MyBeanParam {
+        // Remove this Annotation and use BeanParam directly with Swagger Core 2.0
+    }
 
-	@Api
-	@Path("/")
-	private static class MyBeanParamResource {
-		@GET
-		public String getWithBeanParam(@MyBeanParam ListOfStringsBeanParam listOfStringsBean) {
-			return "result";
-		}
-	}
+    @Api
+    @Path("/")
+    private static class MyBeanParamResource {
+        @GET
+        public String getWithBeanParam(@MyBeanParam ListOfStringsBeanParam listOfStringsBean) {
+            return "result";
+        }
+    }
 
-	private final SwaggerExtension myDefaultParameterExtension = new DefaultParameterExtension() {
-		@Override
-		protected boolean shouldHandleAdditionalAnnotation(Annotation annotation) {
-			return annotation instanceof MyBeanParam;
-		}
-	};
+    private final SwaggerExtension myDefaultParameterExtension = new DefaultParameterExtension() {
+        @Override
+        protected boolean shouldHandleAdditionalAnnotation(Annotation annotation) {
+            return annotation instanceof MyBeanParam;
+        }
+    };
 
-	private List<SwaggerExtension> originalExtensions;
+    private List<SwaggerExtension> originalExtensions;
 
-	@BeforeMethod
-	public void beforeMethod() {
-		originalExtensions = SwaggerExtensions.getExtensions();
-		SwaggerExtensions.setExtensions(Collections.singletonList(myDefaultParameterExtension));
-	}
+    @BeforeMethod
+    public void beforeMethod() {
+        originalExtensions = SwaggerExtensions.getExtensions();
+        SwaggerExtensions.setExtensions(Collections.singletonList(myDefaultParameterExtension));
+    }
 
-	@AfterMethod
-	public void afterMethod() {
-		SwaggerExtensions.setExtensions(originalExtensions);
-	}
+    @AfterMethod
+    public void afterMethod() {
+        SwaggerExtensions.setExtensions(originalExtensions);
+    }
 
-	@Test(description = "check array type of serialized BeanParam containing QueryParams") // tests issue #2466
-	public void shouldSerializeTypeParameter() {
-		Swagger swagger = new Reader(new Swagger()).read(MyBeanParamResource.class);
-		List<Parameter> getOperationParams = swagger.getPath("/").getGet().getParameters();
-		Assert.assertEquals(getOperationParams.size(), 1);
-		QueryParameter param = (QueryParameter) getOperationParams.get(0);
-		Assert.assertEquals(param.getName(), "listOfStrings");
-		Assert.assertEquals(param.getType(), "array");
-		// These are the important checks:
-		Property itemsProperty = param.getItems();
-		Assert.assertEquals(itemsProperty.getClass(), StringProperty.class);
-		Assert.assertEquals(itemsProperty.getType(), "string");
-	}
+    @Test(description = "check array type of serialized BeanParam containing QueryParams") // tests issue #2466
+    public void shouldSerializeTypeParameter() {
+        Swagger swagger = new Reader(new Swagger()).read(MyBeanParamResource.class);
+        List<Parameter> getOperationParams = swagger.getPath("/").getGet().getParameters();
+        Assert.assertEquals(getOperationParams.size(), 1);
+        QueryParameter param = (QueryParameter) getOperationParams.get(0);
+        Assert.assertEquals(param.getName(), "listOfStrings");
+        Assert.assertEquals(param.getType(), "array");
+        // These are the important checks:
+        Property itemsProperty = param.getItems();
+        Assert.assertEquals(itemsProperty.getClass(), StringProperty.class);
+        Assert.assertEquals(itemsProperty.getType(), "string");
+    }
 
 }
