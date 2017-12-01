0a7c89b624b4f2884e87c38e7b6e169cf610f080
pj.fanning <pj.fanning@workday.com>
2017-12-01 18:03:56 +0000

    remove extra import

--- modules/swagger-core/src/main/java/io/swagger/v3/core/jackson/ModelResolver.java
+++ modules/swagger-core/src/main/java/io/swagger/v3/core/jackson/ModelResolver.java
@@ -60,7 +60,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
