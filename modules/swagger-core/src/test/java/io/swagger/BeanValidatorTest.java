80944d4ee51a2e41f96987db1fb8b70f9d894286
Maciej Radzikowski <maciej@radzikowski.com.pl>
2018-01-04 23:10:20 +0100

    Add missing @NotNull bean validation handling test

--- modules/swagger-core/src/test/java/io/swagger/BeanValidatorTest.java
+++ modules/swagger-core/src/test/java/io/swagger/BeanValidatorTest.java
@@ -6,6 +6,7 @@ import io.swagger.models.Model;
 import io.swagger.models.properties.ArrayProperty;
 import io.swagger.models.properties.DoubleProperty;
 import io.swagger.models.properties.IntegerProperty;
+import io.swagger.models.properties.LongProperty;
 import io.swagger.models.properties.Property;
 import io.swagger.models.properties.StringProperty;
 
@@ -22,6 +23,9 @@ public class BeanValidatorTest {
         final Map<String, Model> schemas = ModelConverters.getInstance().readAll(BeanValidationsModel.class);
         final Model model = schemas.get("BeanValidationsModel");
         final Map<String, Property> properties = model.getProperties();
+
+        final LongProperty id = (LongProperty) properties.get("id");
+        Assert.assertTrue(id.getRequired());
 
         final IntegerProperty age = (IntegerProperty) properties.get("age");
         Assert.assertEquals(age.getMinimum(), new BigDecimal(13.0));
