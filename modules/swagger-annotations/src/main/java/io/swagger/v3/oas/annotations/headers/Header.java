34f1bdcb958cdb25c8fba274bc4d9ef049678124
Kazuhiro Sera <seratch@gmail.com>
2018-08-11 01:49:39 +0900

    Fix typo detected by github.com/client9/misspell

--- modules/swagger-annotations/src/main/java/io/swagger/v3/oas/annotations/headers/Header.java
+++ modules/swagger-annotations/src/main/java/io/swagger/v3/oas/annotations/headers/Header.java
@@ -25,7 +25,7 @@ import java.lang.annotation.Target;
 
 /**
  * The annotation may be used to add one or more headers to the definition of a response or as attribute of content
- * encoding by definining it as field {@link io.swagger.v3.oas.annotations.responses.ApiResponse#headers()} or {@link io.swagger.v3.oas.annotations.media.Content#encoding()}.
+ * encoding by defining it as field {@link io.swagger.v3.oas.annotations.responses.ApiResponse#headers()} or {@link io.swagger.v3.oas.annotations.media.Content#encoding()}.
  * <p>Please note that request headers are defined as Header {@link io.swagger.v3.oas.annotations.Parameter}.</p>
  *
  * @see <a target="_new" href="https://github.com/OAI/OpenAPI-Specification/blob/3.0.1/versions/3.0.1.md#headerObject">Header (OpenAPI specification)</a>
