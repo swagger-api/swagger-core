bb6e1a8584c1d5f83f7a61fc403c1cc6f6e51070
matthew_hoover <matthew.hoover@oracle.com>
2019-07-29 12:18:35 -0700

    Per Issue 3257 here is an attempt at a fix
    This exposes the internal required field for external spec validation.
    With this code the caller sees the actual fields needed in the spec vs
    what the swagger parser produced.
    
    Signed-off-by: matthew_hoover <matthew.hoover@oracle.com>

--- modules/swagger-models/src/main/java/io/swagger/models/ModelImpl.java
+++ modules/swagger-models/src/main/java/io/swagger/models/ModelImpl.java
@@ -433,4 +433,9 @@ public class ModelImpl extends AbstractModel {
         return cloned;
     }
 
+    @JsonIgnore
+    public List<String> getSpecSpecifiedRequired() {
+        return required;
+    }
+
 }
