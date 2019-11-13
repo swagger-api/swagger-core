32b9cd8526fa6744dcd1434f6d9febaa40f44ff4
frantuma <frantuma@yahoo.com>
2019-11-13 18:56:57 +0100

    bump jackson to 2.10.1 - fixes CVE

--- modules/swagger-jaxrs2/src/test/java/io/swagger/v3/jaxrs2/resources/RefParameter3074Resource.java
+++ modules/swagger-jaxrs2/src/test/java/io/swagger/v3/jaxrs2/resources/RefParameter3074Resource.java
@@ -19,12 +19,12 @@ public class RefParameter3074Resource {
             "        summary: Get an employee\n" +
             "        operationId: getEmployee\n" +
             "        responses:\n" +
-            "          200:\n" +
+            "          \"200\":\n" +
             "            content:\n" +
             "              application/json:\n" +
             "                schema:\n" +
             "                   $ref: '#/components/schemas/Employee'\n" +
-            "          500:\n" +
+            "          \"500\":\n" +
             "            content:\n" +
             "              application/json:\n" +
             "                schema:\n" +
