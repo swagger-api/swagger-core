5ee1151cb16d8f0fefc348c7590fade2b3f0eac4
Espen Fossen <espenaf@junta.no>
2020-05-29 13:57:22 +0200

    Added support for classFilter to OpenApiServlet

--- modules/swagger-jaxrs2/src/main/java/io/swagger/v3/jaxrs2/integration/OpenApiServlet.java
+++ modules/swagger-jaxrs2/src/main/java/io/swagger/v3/jaxrs2/integration/OpenApiServlet.java
@@ -1,5 +1,7 @@
 package io.swagger.v3.jaxrs2.integration;
 
+import io.swagger.v3.core.filter.OpenAPISpecFilter;
+import io.swagger.v3.core.filter.SpecFilter;
 import io.swagger.v3.core.util.Json;
 import io.swagger.v3.core.util.Yaml;
 import io.swagger.v3.oas.integration.OpenApiConfigurationException;
@@ -7,18 +9,29 @@ import io.swagger.v3.oas.integration.OpenApiContextLocator;
 import io.swagger.v3.oas.integration.api.OpenApiContext;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static io.swagger.v3.jaxrs2.integration.ServletConfigContextUtils.getContextIdFromServletConfig;
 
 public class OpenApiServlet extends HttpServlet {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(OpenApiServlet.class);
 
     public static final String APPLICATION_JSON = "application/json";
     public static final String APPLICATION_YAML = "application/yaml";
@@ -47,6 +60,19 @@ public class OpenApiServlet extends HttpServlet {
         String ctxId = getContextIdFromServletConfig(getServletConfig());
         OpenApiContext ctx = OpenApiContextLocator.getInstance().getOpenApiContext(ctxId);
         OpenAPI oas = ctx.read();
+
+        if (oas != null) {
+            if (ctx.getOpenApiConfiguration() != null && ctx.getOpenApiConfiguration().getFilterClass() != null) {
+                try {
+                    OpenAPISpecFilter filterImpl = (OpenAPISpecFilter) Class.forName(ctx.getOpenApiConfiguration().getFilterClass()).newInstance();
+                    SpecFilter f = new SpecFilter();
+                    oas = f.filter(oas, filterImpl, getQueryParams(req.getQueryString()), getCookies(req.getCookies()),
+                            getHeaders(req));
+                } catch (Exception e) {
+                    LOGGER.error("failed to load filter", e);
+                }
+            }
+        }
 
         String type = "json";
 
@@ -79,5 +105,44 @@ public class OpenApiServlet extends HttpServlet {
             pw.close();
         }
 
+    }
+
+    public static Map<String, List<String>> getQueryParams(String queryString) {
+        Map<String, List<String>> queryParameters = new HashMap<>();
+
+        if (StringUtils.isEmpty(queryString)) {
+            return queryParameters;
+        }
+
+        String[] parameters = queryString.split("&");
+
+        for (String parameter : parameters) {
+            String[] keyValuePair = parameter.split("=");
+            queryParameters.put(keyValuePair[0], Collections.singletonList(keyValuePair[1]));
+        }
+        return queryParameters;
+    }
+
+    private static Map<String, String> getCookies(Cookie[] cookies) {
+        Map<String, String> mapOfCookies = new HashMap<>();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                mapOfCookies.put(cookie.getName(), cookie.getValue());
+            }
+        }
+        return mapOfCookies;
+    }
+
+    public static Map<String, List<String>> getHeaders(HttpServletRequest req) {
+        if (req.getHeaderNames() == null) {
+            return new HashMap<>();
+        } else {
+            return Collections
+                    .list(req.getHeaderNames())
+                    .stream()
+                    .collect(Collectors.toMap(
+                            Function.identity(),
+                            header -> Collections.list(req.getHeaders(header))));
+        }
     }
 }
