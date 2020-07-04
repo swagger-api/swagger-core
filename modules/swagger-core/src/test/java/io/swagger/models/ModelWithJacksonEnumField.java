40b8b27d8880c41dadd4b8505ba22444500a1960
Hardhat <hard.hat@gmx.de>
2020-07-05 01:17:38 +0200

    Backport from master of issue #3552 for support to override enum values with @JsonProperty or @JsonValue

---
+++ modules/swagger-core/src/test/java/io/swagger/models/ModelWithJacksonEnumField.java
@@ -0,0 +1,7 @@
+package io.swagger.models;
+
+public class ModelWithJacksonEnumField {
+    public JacksonPropertyEnum firstEnumValue;
+    public JacksonValueEnum secondEnumValue;
+    public JacksonNumberValueEnum thirdEnumValue;
+}
