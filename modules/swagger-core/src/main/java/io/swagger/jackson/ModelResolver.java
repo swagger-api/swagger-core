340987cd952001998b0b4d836754c2a2efc8a004
Hardhat <hard.hat@gmx.de>
2020-07-05 02:30:37 +0200

    Removed unused and unsupported Optional import.

--- modules/swagger-core/src/main/java/io/swagger/jackson/ModelResolver.java
+++ modules/swagger-core/src/main/java/io/swagger/jackson/ModelResolver.java
@@ -14,7 +14,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 import javax.validation.constraints.DecimalMax;
--- modules/swagger-core/src/main/java/io/swagger/util/ReflectionUtils.java
+++ modules/swagger-core/src/main/java/io/swagger/util/ReflectionUtils.java
@@ -12,7 +12,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 import org.apache.commons.lang3.ArrayUtils;
