ae39028338eb11ff918a5f48bf4993cdae5e1fae
Birgit Kratz <mail@birgitkratz.de>
2020-07-20 12:13:48 +0200

    #2092: Support for overriding enum values with @XmlEnumValue

---
+++ modules/swagger-core/src/test/java/io/swagger/v3/core/oas/models/Model2092.java
@@ -0,0 +1,15 @@
+package io.swagger.v3.core.oas.models;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Model2092 {
+    private TestEnum2092 enumValue;
+
+    public TestEnum2092 getEnumValue() {
+        return enumValue;
+    }
+    public void setEnumValue(TestEnum2092 e) {
+        this.enumValue = e;
+    }
+}
