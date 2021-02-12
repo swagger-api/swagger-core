9e1ac33a0a8cc6b98cb4690590bc1d2e5f93994b
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-02-12 20:12:58 +0100

    Resolves #3879
    Fix @JsonUnwrapped does not copy required properties

--- modules/swagger-core/src/main/java/io/swagger/v3/core/jackson/ModelResolver.java
+++ modules/swagger-core/src/main/java/io/swagger/v3/core/jackson/ModelResolver.java
@@ -124,6 +124,7 @@ public class ModelResolver extends AbstractModelConverter implements ModelConver
 
         boolean isPrimitive = false;
         Schema model = null;
+        List<String> requiredProps = new ArrayList<>();
 
         if (annotatedType == null) {
             return null;
@@ -643,7 +644,7 @@ public class ModelResolver extends AbstractModelConverter implements ModelConver
                             .ctxAnnotations(null)
                             .jsonUnwrappedHandler(null)
                             .resolveAsRef(false);
-                        handleUnwrapped(props, context.resolve(t), uw.prefix(), uw.suffix());
+                        handleUnwrapped(props, context.resolve(t), uw.prefix(), uw.suffix(), requiredProps);
                         return null;
                     } else {
                         return new Schema();
@@ -708,6 +709,9 @@ public class ModelResolver extends AbstractModelConverter implements ModelConver
         }
         if (modelProps.size() > 0) {
             model.setProperties(modelProps);
+            for(String propName : requiredProps) {
+                addRequiredItem(model, propName);
+            }
         }
 
         /**
@@ -1019,10 +1023,14 @@ public class ModelResolver extends AbstractModelConverter implements ModelConver
         return false;
     }
 
-    private void handleUnwrapped(List<Schema> props, Schema innerModel, String prefix, String suffix) {
+    private void handleUnwrapped(List<Schema> props, Schema innerModel, String prefix, String suffix, List<String> requiredProps) {
         if (StringUtils.isBlank(suffix) && StringUtils.isBlank(prefix)) {
             if (innerModel.getProperties() != null) {
                 props.addAll(innerModel.getProperties().values());
+                if(innerModel.getRequired() != null) {
+                    requiredProps.addAll(innerModel.getRequired());
+                }
+
             }
         } else {
             if (prefix == null) {
