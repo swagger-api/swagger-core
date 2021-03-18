afc878a4b3cb7e537b1a88f6e886a2a020abc236
Alex Kröker <5667523+AlexKroeker@users.noreply.github.com>
2021-03-18 09:57:17 +0100

    Remove copyright

--- modules/swagger-jaxrs2/src/test/java/io/swagger/v3/jaxrs2/resources/model/MultipleSubSub1Bean.java
+++ modules/swagger-jaxrs2/src/test/java/io/swagger/v3/jaxrs2/resources/model/MultipleSubSub1Bean.java
@@ -1,17 +1,8 @@
-/*******************************************************************************
- * Copyright (c) since 2010 by
- *
- * synavision GmbH
- * http://www.synavision.de
- *
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * All rights reserved.
- *******************************************************************************/
 package io.swagger.v3.jaxrs2.resources.model;
 
-/**
- * TODOC
- */
+@io.swagger.v3.oas.annotations.media.Schema(
+        description = "MultipleSubSub1Bean"
+)
 public class MultipleSubSub1Bean extends MultipleSub1Bean {
 
     public java.util.List<MultipleBaseBean> beans;
