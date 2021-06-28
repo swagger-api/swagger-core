a572e1fad67e91519254522717d012ada7c1a2f4
GitHub Action <action@github.com>
2021-06-28 08:36:39 +0000

    bump snapshot 2.1.11-SNAPSHOT

--- modules/swagger-gradle-plugin/src/test/java/io/swagger/v3/plugins/gradle/SwaggerResolveTest.java
+++ modules/swagger-gradle-plugin/src/test/java/io/swagger/v3/plugins/gradle/SwaggerResolveTest.java
@@ -82,7 +82,7 @@ public class SwaggerResolveTest {
                 "    mavenCentral()\n" +
                 "}\n" +
                 "dependencies {  \n" +
-                "    compile group: 'io.swagger.core.v3', name: 'swagger-jaxrs2', version:'2.1.10'\n" +
+                "    compile group: 'io.swagger.core.v3', name: 'swagger-jaxrs2', version:'2.1.11-SNAPSHOT'\n" +
                 "    compile group: 'javax.ws.rs', name: 'javax.ws.rs-api', version:'2.1'\n" +
                 "    compile group: 'javax.servlet', name: 'javax.servlet-api', version:'3.1.0'\n" +
                 "    testCompile group: 'com.github.tomakehurst', name: 'wiremock', version:'2.27.2'\n" +
