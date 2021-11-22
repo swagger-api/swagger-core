9b070acfed00b3510f5b3a1c52f56c505f44db9b
Hugo Mercado <hugo.mario.mercado@gmail.com>
2021-11-22 04:37:28 -0500

    added mixin to handle new openapi 3.1 fields: info.summary, license.identifier, webhooks, components.pahitems

--- modules/swagger-core/src/main/java/io/swagger/v3/core/jackson/mixin/OpenAPIMixin.java
+++ modules/swagger-core/src/main/java/io/swagger/v3/core/jackson/mixin/OpenAPIMixin.java
@@ -2,8 +2,10 @@ package io.swagger.v3.core.jackson.mixin;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import io.swagger.v3.core.jackson.PathsSerializer;
+import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.Paths;
 import java.util.Map;
 
@@ -17,4 +19,7 @@ public abstract class OpenAPIMixin {
 
     @JsonSerialize(using = PathsSerializer.class)
     public abstract Paths getPaths();
+
+    @JsonIgnore
+    public abstract Map<String, PathItem> getWebhooks();
 }
