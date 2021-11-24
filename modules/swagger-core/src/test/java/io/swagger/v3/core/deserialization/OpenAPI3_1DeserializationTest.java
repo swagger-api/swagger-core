fe7a292aaa30a5083421c0c328f05d213e2b9a43
Hugo Mercado <hugo.mario.mercado@gmail.com>
2021-11-23 19:00:33 -0500

    added deserialization tests

--- modules/swagger-core/src/test/java/io/swagger/v3/core/deserialization/OpenAPI3_1DeserializationTest.java
+++ modules/swagger-core/src/test/java/io/swagger/v3/core/deserialization/OpenAPI3_1DeserializationTest.java
@@ -9,8 +9,11 @@ import org.testng.annotations.Test;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertNull;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
 
 public class OpenAPI3_1DeserializationTest {
 
@@ -51,5 +54,45 @@ public class OpenAPI3_1DeserializationTest {
         assertEquals(swagger.getInfo().getLicense().getIdentifier(), "test");
         Yaml31.prettyPrint(swagger);
         SerializationMatchers.assertEqualsToYaml31(swagger, jsonString);
+    }
+
+    @Test
+    public void testDeserializationOnOAS31() throws IOException {
+
+        final String jsonString = ResourceUtils.loadClassResource(getClass(), "specFiles/3.1.0/petstore-3.1_sample.yaml");
+        OpenAPI openAPI = Yaml31.mapper().readValue(jsonString, OpenAPI.class);
+        assertNotNull(openAPI);
+
+        assertEquals(openAPI.getInfo().getTitle(), "Swagger Petstore");
+        assertEquals(openAPI.getInfo().getVersion(), "1.0.0");
+        assertEquals(openAPI.getInfo().getSummary(), "petstore sample for OAS 3.1.0");
+        assertEquals(openAPI.getInfo().getLicense().getName(), "MIT");
+        assertEquals(openAPI.getInfo().getLicense().getIdentifier(), "test");
+
+        assertNotNull(openAPI.getWebhooks());
+        assertFalse(openAPI.getWebhooks().isEmpty());
+        assertNotNull(openAPI.getWebhooks().get("newPet"));
+        assertNotNull(openAPI.getWebhooks().get("newPet").getPost());
+
+        assertNotNull(openAPI.getComponents().getPathItems());
+        assertNotNull(openAPI.getComponents().getPathItems().get("/pet"));
+        assertEquals(openAPI.getComponents().getPathItems().get("/pet").getDescription(), "get a pet");
+        assertNotNull(openAPI.getComponents().getPathItems().get("/pet").getGet());
+        assertEquals(openAPI.getComponents().getPathItems().get("/pet").getGet().getOperationId(), "getPet");
+
+        assertNotNull(openAPI.getComponents().getSchemas());
+        assertNotNull(openAPI.getComponents().getSchemas().get("Pet"));
+        assertNotNull(openAPI.getComponents().getSchemas().get("Pet").getDiscriminator());
+        assertNotNull(openAPI.getComponents().getSchemas().get("Pet").getDiscriminator().getExtensions());
+        assertEquals(openAPI.getComponents().getSchemas().get("Pet").getDiscriminator().getExtensions().get("x-test-extension"), "extended");
+
+        openAPI = Yaml.mapper().readValue(jsonString, OpenAPI.class);
+        assertNotNull(openAPI);
+
+        assertNull(openAPI.getInfo().getSummary());
+        assertNull(openAPI.getWebhooks());
+        assertNull(openAPI.getComponents().getPathItems());
+        assertNull(openAPI.getComponents().getSchemas().get("Pet").getDiscriminator().getExtensions());
+
     }
 }
