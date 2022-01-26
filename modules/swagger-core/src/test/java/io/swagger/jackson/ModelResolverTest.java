172d218337ac2d855c868019ed3d29f5da7be9cd
dpeger <dpeger@users.noreply.github.com>
2022-01-26 16:59:07 +0100

    #4103 Support for Jackson reference types
    In addition to the hard coded `Optional` reference types the type information from jackson is used to unwrap additional reference types (e.g. `AtomicReference`)

--- modules/swagger-core/src/test/java/io/swagger/jackson/ModelResolverTest.java
+++ modules/swagger-core/src/test/java/io/swagger/jackson/ModelResolverTest.java
@@ -1,16 +1,25 @@
 package io.swagger.jackson;
 
+import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import io.swagger.converter.ModelConverterContextImpl;
 import io.swagger.models.Link;
 import io.swagger.models.Model;
 import io.swagger.models.ModelImpl;
 import io.swagger.models.Xml;
+import io.swagger.models.properties.DecimalProperty;
+import io.swagger.models.properties.Property;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import javax.xml.bind.annotation.XmlRootElement;
+
+import java.lang.annotation.Annotation;
+import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.testng.Assert.*;
 
@@ -50,5 +59,40 @@ public class ModelResolverTest extends SwaggerTestBase {
     @XmlRootElement(name = "TypeWithOutNamespace")
     public static class TypeWithoutNamespace {
     }
+
+    @Test
+    public void testResolvePropertyWithAtomicReference() {
+        final ObjectMapper mapper = mapper();
+        final ModelResolver modelResolver = new ModelResolver(mapper);
+
+        final JavaType javaType = TypeFactory.defaultInstance().constructType(TypeWithAtomicReferenceMember.class);
+        final BeanDescription beanDescription = mapper.getSerializationConfig().introspect(javaType);
+
+        JavaType atomicReferenceBigDecimalType = null;
+        for (final BeanPropertyDefinition propDef : beanDescription.findProperties()) {
+            if ("member".equals(propDef.getName())) {
+                atomicReferenceBigDecimalType = propDef.getPrimaryType();
+            }
+        }
+        assertNotNull(atomicReferenceBigDecimalType, "Failed to read atomic reference field 'member'");
+
+        final Property actualProperty = modelResolver.resolveProperty(atomicReferenceBigDecimalType,
+                new ModelConverterContextImpl(modelResolver), new Annotation[0], null);
+        assertEquals(actualProperty.getType(), DecimalProperty.TYPE, "Got wrong type for AtomicReference member");
+    }
+
+    @SuppressWarnings("unused")
+    private static final class TypeWithAtomicReferenceMember {
+        AtomicReference<BigDecimal> member;
+
+        public AtomicReference<BigDecimal> getMember() {
+            return member;
+        }
+
+        public void setMember(AtomicReference<BigDecimal> member) {
+            this.member = member;
+        }
+    }
+
 
 }
