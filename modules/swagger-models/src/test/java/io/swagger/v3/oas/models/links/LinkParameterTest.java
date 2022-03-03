72cc2e88fa637f53f00fe565645e00548949f0f9
Hugo Mercado <hugo.mario.mercado@gmail.com>
2022-03-02 21:50:41 -0500

    added/updated copyright documentation on swagger-modles module files

--- modules/swagger-models/src/test/java/io/swagger/v3/oas/models/links/LinkParameterTest.java
+++ modules/swagger-models/src/test/java/io/swagger/v3/oas/models/links/LinkParameterTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019 SmartBear Software
+ * Copyright 2021 SmartBear Software
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
