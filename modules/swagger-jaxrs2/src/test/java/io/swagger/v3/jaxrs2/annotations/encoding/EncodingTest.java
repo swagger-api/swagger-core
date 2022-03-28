df0f4c13ffe6b39ce014f1276548468d182aafde
frantuma <frantuma@yahoo.com>
2022-03-28 21:49:30 +0200

    remove copyright header

--- modules/swagger-jaxrs2/src/test/java/io/swagger/v3/jaxrs2/annotations/encoding/EncodingTest.java
+++ modules/swagger-jaxrs2/src/test/java/io/swagger/v3/jaxrs2/annotations/encoding/EncodingTest.java
@@ -1,14 +1,3 @@
-/*
-* IBM Confidential
-*
-* OCO Source Materials
-*
-* WLP Copyright IBM Corp. 2017
-*
-* The source code for this program is not published or otherwise divested
-* of its trade secrets, irrespective of what has been deposited with the
-* U.S. Copyright Office.
-*/
 package io.swagger.v3.jaxrs2.annotations.encoding;
 
 import io.swagger.v3.jaxrs2.annotations.AbstractAnnotationTest;
