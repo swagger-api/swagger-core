c4472547b2ffbb52aaa0194ac8febdf0067a87cc
frantuma <frantuma@yahoo.com>
2022-04-04 13:23:10 +0200

    refs #4059 - don't ignore with @JsonIgnore(false)

--- modules/swagger-core/src/main/java/io/swagger/v3/core/jackson/ModelResolver.java
+++ modules/swagger-core/src/main/java/io/swagger/v3/core/jackson/ModelResolver.java
@@ -1009,7 +1009,7 @@ public class ModelResolver extends AbstractModelConverter implements ModelConver
         if (propertiesToIgnore.contains(propName)) {
             return true;
         }
-        if (member.hasAnnotation(JsonIgnore.class)) {
+        if (member.hasAnnotation(JsonIgnore.class) && member.getAnnotation(JsonIgnore.class).value()) {
             return true;
         }
         if (hasHiddenAnnotation(member)) {
