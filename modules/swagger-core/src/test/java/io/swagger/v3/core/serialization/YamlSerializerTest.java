7dc50f3fb42ce0afeeafdb4386ebc0c5286c5b1f
Hugo Mercado <hugo.mario.mercado@gmail.com>
2023-01-13 00:22:38 -0500

    Implemented a string quoting checked class to stop the backslash addition issue when a comma is present in a text.

--- modules/swagger-core/src/test/java/io/swagger/v3/core/serialization/YamlSerializerTest.java
+++ modules/swagger-core/src/test/java/io/swagger/v3/core/serialization/YamlSerializerTest.java
@@ -2,7 +2,12 @@ package io.swagger.v3.core.serialization;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import io.swagger.v3.core.util.Yaml;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
 
 import static org.testng.Assert.assertEquals;
 
@@ -68,5 +73,31 @@ public class YamlSerializerTest {
         String serializedYaml = Yaml.pretty(node);
 
         assertEquals(serializedYaml, yaml);
+    }
+
+    @Test
+    public void testNoBackSlashesAddedInYaml() throws Exception {
+        OpenAPI openAPI = new OpenAPI()
+                .openapi("3.0.0")
+                .info(new Info()
+                        .title("Test long description")
+                        .version("1.0.0")
+                        .description("Lorem ipsum dolor sit amet consectetur adipiscing elit, ligula integer tempus proin mattis fusce. Proin vivamus phasellus tincidunt sapien inceptos viverra turpis vehicula neque sociosqu, nostra rhoncus sagittis cursus dapibus dignissim ornare consequat curabitur, imperdiet lacus facilisis ridiculus ante molestie convallis accumsan donec. Laoreet pulvinar feugiat velit proin rhoncus dis ullamcorper, curae nullam at penatibus tempor quis."))
+                .servers(Arrays.asList(new Server().url("/")));
+
+        String serializedYaml = Yaml.pretty(openAPI);
+
+        assertEquals(serializedYaml, "openapi: 3.0.0\n" +
+                "info:\n" +
+                "  title: Test long description\n" +
+                "  description: Lorem ipsum dolor sit amet consectetur adipiscing elit, ligula integer\n" +
+                "    tempus proin mattis fusce. Proin vivamus phasellus tincidunt sapien inceptos viverra\n" +
+                "    turpis vehicula neque sociosqu, nostra rhoncus sagittis cursus dapibus dignissim\n" +
+                "    ornare consequat curabitur, imperdiet lacus facilisis ridiculus ante molestie\n" +
+                "    convallis accumsan donec. Laoreet pulvinar feugiat velit proin rhoncus dis ullamcorper,\n" +
+                "    curae nullam at penatibus tempor quis.\n" +
+                "  version: 1.0.0\n" +
+                "servers:\n" +
+                "- url: /\n");
     }
 }
