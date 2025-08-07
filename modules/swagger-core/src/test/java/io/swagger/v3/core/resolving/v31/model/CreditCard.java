fbe8a8dab51175c143ecd2d9c2ddb42b8cdedb17
Ewa Ostrowska <ewa.ostrowska@smartbear.com>
2025-08-07 11:52:11 +0200

    fix: Fix missing items on nested @ArraySchema items in OAS 3.1 Resolves #4944 #4836

--- modules/swagger-core/src/test/java/io/swagger/v3/core/resolving/v31/model/CreditCard.java
+++ modules/swagger-core/src/test/java/io/swagger/v3/core/resolving/v31/model/CreditCard.java
@@ -1,8 +1,15 @@
 package io.swagger.v3.core.resolving.v31.model;
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.Set;
+
 public class CreditCard {
 
     private String billingAddress;
+    private Set<Address.CountryEnum> acceptingCountries;
+    private Set<Currency> availableCurrencies;
 
     public String getBillingAddress() {
         return billingAddress;
@@ -10,5 +17,23 @@ public class CreditCard {
 
     public void setBillingAddress(String billingAddress) {
         this.billingAddress = billingAddress;
+    }
+
+    @ArraySchema(schema = @Schema(description = "accepting country"))
+    public Set<Address.CountryEnum> getAcceptingCountries() {
+        return acceptingCountries;
+    }
+
+    public void setAcceptingCountries(Set<Address.CountryEnum> acceptingCountries) {
+        this.acceptingCountries = acceptingCountries;
+    }
+
+    @ArraySchema(schema = @Schema(description = "available currencies"))
+    public Set<Currency> getAvailableCurrencies() {
+        return availableCurrencies;
+    }
+
+    public void setAvailableCurrencies(Set<Currency> availableCurrencies) {
+        this.availableCurrencies = availableCurrencies;
     }
 }
