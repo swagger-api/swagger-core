50bf7d7176b9a5dd5cf368a957fea42e68f13d05
Mattias-Sehlstedt <60173714+Mattias-Sehlstedt@users.noreply.github.com>
2026-05-04 18:19:35 +0200

    fix: negative and positive validation annotations uses relevant OAS 3.1 syntax

--- modules/swagger-core/src/test/java/io/swagger/v3/core/resolving/BeanValidatorTest.java
+++ modules/swagger-core/src/test/java/io/swagger/v3/core/resolving/BeanValidatorTest.java
@@ -2,12 +2,7 @@ package io.swagger.v3.core.resolving;
 
 import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.core.oas.models.BeanValidationsModel;
-import io.swagger.v3.oas.models.media.ArraySchema;
-import io.swagger.v3.oas.models.media.EmailSchema;
-import io.swagger.v3.oas.models.media.IntegerSchema;
-import io.swagger.v3.oas.models.media.NumberSchema;
-import io.swagger.v3.oas.models.media.Schema;
-import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.media.*;
 import org.testng.annotations.Test;
 
 import java.math.BigDecimal;
@@ -20,7 +15,7 @@ import static org.testng.Assert.assertTrue;
 public class BeanValidatorTest {
 
     @Test(description = "read bean validations")
-    public void readBeanValidatorTest() {
+    public void readBeanValidatorOAS30Test() {
         final Map<String, Schema> schemas = ModelConverters.getInstance().readAll(BeanValidationsModel.class);
         final Schema model = schemas.get("BeanValidationsModel");
         final Map<String, Schema> properties = model.getProperties();
@@ -93,5 +88,84 @@ public class BeanValidatorTest {
         final NumberSchema negativeOrZeroWithMax = (NumberSchema) properties.get("negativeOrZeroWithMax");
         assertEquals(negativeOrZeroWithMax.getMaximum(), new BigDecimal("-2"));
         assertNull(negativeOrZeroWithMax.getExclusiveMaximum());
+    }
+
+    @Test(description = "read bean validations")
+    public void readBeanValidatorOAS31Test() {
+        final Map<String, Schema> schemas = ModelConverters.getInstance(true).readAll(BeanValidationsModel.class);
+        final Schema model = schemas.get("BeanValidationsModel");
+        final Map<String, Schema> properties = model.getProperties();
+
+        assertTrue(model.getRequired().contains("id"));
+        assertTrue(model.getRequired().contains("username"));
+
+        final JsonSchema username = (JsonSchema) properties.get("username");
+        assertEquals(username.getPattern(), "(?![-._])[-._a-zA-Z0-9]{3,32}");
+
+        final JsonSchema age = (JsonSchema) properties.get("age");
+        assertEquals(age.getMinimum(), new BigDecimal(13.0));
+        assertEquals(age.getMaximum(), new BigDecimal(99.0));
+
+        final JsonSchema password = (JsonSchema) properties.get("password");
+        assertEquals((int) password.getMinLength(), 6);
+        assertEquals((int) password.getMaxLength(), 20);
+
+        final JsonSchema email = (JsonSchema) properties.get("email");
+        assertEquals(email.getFormat(), "email");
+
+        final JsonSchema minBalance = (JsonSchema) properties.get("minBalance");
+        assertTrue(minBalance.getExclusiveMinimum());
+
+        final JsonSchema maxBalance = (JsonSchema) properties.get("maxBalance");
+        assertTrue(maxBalance.getExclusiveMaximum());
+
+        final JsonSchema items = (JsonSchema) properties.get("items");
+        assertEquals((int) items.getMinItems(), 2);
+        assertEquals((int) items.getMaxItems(), 10);
+        assertEquals((int) items.getItems().getMinLength(), 3);
+        assertEquals((int) items.getItems().getMaxLength(), 4);
+
+        final JsonSchema optionalValue = (JsonSchema) properties.get("optionalValue");
+        assertEquals((int) optionalValue.getMinLength(), 1);
+        assertEquals((int) optionalValue.getMaxLength(), 10);
+
+        final JsonSchema positiveAmount = (JsonSchema) properties.get("positiveAmount");
+        assertEquals(positiveAmount.getExclusiveMinimumValue(), BigDecimal.ZERO);
+
+        final JsonSchema positiveOrZeroAmount = (JsonSchema) properties.get("positiveOrZeroAmount");
+        assertEquals(positiveOrZeroAmount.getMinimum(), BigDecimal.ZERO);
+        assertNull(positiveOrZeroAmount.getExclusiveMinimum());
+
+        final JsonSchema negativeAmount = (JsonSchema) properties.get("negativeAmount");
+        assertEquals(negativeAmount.getExclusiveMaximumValue(), BigDecimal.ZERO);
+
+        final JsonSchema negativeOrZeroAmount = (JsonSchema) properties.get("negativeOrZeroAmount");
+        assertEquals(negativeOrZeroAmount.getMaximum(), BigDecimal.ZERO);
+        assertNull(negativeOrZeroAmount.getExclusiveMaximum());
+
+        final JsonSchema positiveWithMin = (JsonSchema) properties.get("positiveWithMin");
+        assertEquals(positiveWithMin.getMinimum(), new BigDecimal("5"));
+        assertNull(positiveWithMin.getExclusiveMinimum());
+        assertNull(positiveWithMin.getExclusiveMinimumValue());
+
+        final JsonSchema positiveWithDecimalMin = (JsonSchema) properties.get("positiveWithDecimalMin");
+        assertEquals(positiveWithDecimalMin.getMinimum(), new BigDecimal("5.5"));
+        assertNull(positiveWithMin.getExclusiveMinimum());
+        assertNull(positiveWithMin.getExclusiveMinimumValue());
+
+        final JsonSchema positiveOrZeroWithMin = (JsonSchema) properties.get("positiveOrZeroWithMin");
+        assertEquals(positiveOrZeroWithMin.getMinimum(), new BigDecimal("3"));
+        assertNull(positiveWithMin.getExclusiveMinimum());
+        assertNull(positiveWithMin.getExclusiveMinimumValue());
+
+        final JsonSchema negativeWithMax = (JsonSchema) properties.get("negativeWithMax");
+        assertEquals(negativeWithMax.getMaximum(), new BigDecimal("-3"));
+        assertNull(positiveWithMin.getExclusiveMaximum());
+        assertNull(positiveWithMin.getExclusiveMaximumValue());
+
+        final JsonSchema negativeOrZeroWithMax = (JsonSchema) properties.get("negativeOrZeroWithMax");
+        assertEquals(negativeOrZeroWithMax.getMaximum(), new BigDecimal("-2"));
+        assertNull(positiveWithMin.getExclusiveMaximum());
+        assertNull(positiveWithMin.getExclusiveMaximumValue());
     }
 }
