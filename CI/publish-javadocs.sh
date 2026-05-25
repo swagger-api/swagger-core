e9301321875b444fdb97cda13e56f7ab1b842bc1
Vasily Pelikh <2010720+vpelikh@users.noreply.github.com>
2026-05-25 14:17:36 +0300

    Remove wiki update steps and add latest javadoc redirect (#18)
    - Delete wiki checkout and update steps from release workflow
    - Copy javadocs to a "latest" folder in gh-pages for a stable
      URL pointing to the most recent release

--- CI/publish-javadocs.sh
+++ CI/publish-javadocs.sh
@@ -9,8 +9,15 @@ SC_RELEASE_TAG="v${RELEASE_VERSION}"
 ### publish javadocs
 #####################
 
+# Publish versioned javadocs
 mkdir -p $CUR/swagger-core/${SC_RELEASE_TAG}
 cp -aR $TMPDIR/apidocs $CUR/swagger-core/${SC_RELEASE_TAG}
+
+# Publish to "latest" (always points to the most recent release)
+rm -rf $CUR/swagger-core/latest
+mkdir -p $CUR/swagger-core/latest
+cp -aR $TMPDIR/apidocs $CUR/swagger-core/latest
+
 git add -A
 git commit -m "apidocs for release ${SC_RELEASE_TAG}"
 git push -u origin gh-pages
